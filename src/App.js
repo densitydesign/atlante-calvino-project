bedac39c7db4f19dd8ebcc9344d82db761090d22
crillion <crillion@tiscali.it>
2019-11-21 19:35:45 +0100

    atlante-calvino / initial stubs for the hamburger menu and some related view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
-import PlacesMatrixView from './PlacesMatrixView/PlacesMatrixView';
+//import PlacesMatrixView from './PlacesMatrixView/PlacesMatrixView';
 // import ContainerComp from './ContainerComp/ContainerComp';
+import ViewSelector from './views/ViewSelector/ViewSelector';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import PhenomenaIntro from './views/PhenomenaIntro/PhenomenaIntro';
 
 function App() {
   return (
-      <PlacesMatrixView />
+    
+    /*      <PlacesMatrixView /> */
+    <BrowserRouter>
+
+      <ViewSelector />
+
+      <Switch>
+
+        <Route path="/PhenomenaIntro"><PhenomenaIntro /></Route>
+
+      </Switch>
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/views/HamburgerMenu/HamburgerMenu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class HamburgerMenu extends React.Component
+{
+  render()
+  {
+    
+  }
+}
