8852ab63ea19f767b127abb19c693a425db2d9df
iosonosempreio <tommasoelli@Tommasos-MacBook-Pro-2.local>
2019-11-22 01:04:13 -0500

    global data

---
+++ src/GlobalData.js
@@ -0,0 +1,29 @@
+const GlobalData = {
+  allVolumes: [
+    {"id":"V001","label":"Il sentiero dei nidi di ragno"},
+    {"id":"V002","label":"Ultimo viene il corvo"},
+    {"id":"V003","label":"Il visconte dimezzato"},
+    {"id":"V004","label":"L'entrata in guerra"},
+    {"id":"V005","label":"Il barone rampante"},
+    {"id":"V006","label":"I racconti"},
+    {"id":"V007","label":"La formica argentina"},
+    {"id":"V008","label":"Il cavaliere inesistente"},
+    {"id":"V009","label":"La giornata di uno scrutatore"},
+    {"id":"V010","label":"La speculazione edilizia"},
+    {"id":"V011","label":"Marcovaldo"},
+    {"id":"V012","label":"La nuvola di smog e La formica argentina"},
+    {"id":"V013","label":"Le cosmicomiche"},
+    {"id":"V014","label":"Ti con Zero"},
+    {"id":"V015","label":"La memoria del mondo"},
+    {"id":"V016","label":"Il castello dei destini incrociati"},
+    {"id":"V017","label":"Gli amori difficili"},
+    {"id":"V018","label":"Le città invisibili"},
+    {"id":"V019","label":"Il castello dei destini incrociati (riediz)"},
+    {"id":"V020","label":"Eremita a Parigi"},
+    {"id":"V021","label":"Se una notte d\'inverno un viaggiatore"},
+    {"id":"V022","label":"Palomar"},
+    {"id":"V023","label":"Le cosmicomiche vecchie e nuove"}
+  ]
+}
+
+export default GlobalData
