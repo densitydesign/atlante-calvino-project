dde14835de0be657a8f0956a7db82cd3ca8441fe
crillion <crillion@tiscali.it>
2019-11-25 15:10:29 +0100

    hamburger menu : centered text in view selector cells

--- src/views/HamburgerMenu/HamburgerMenu.js
+++ src/views/HamburgerMenu/HamburgerMenu.js
@@ -12,29 +12,30 @@ export default class HamburgerMenu extends React.Component
     return (
       <div className="hamburger-menu">
         <div className="hamburger-menu-sidebar">
-          <ViewSelector route="Compass" text="Bussola" />
+          <ViewSelector className="hamburger-menu-cell" route="Compass" text="Bussola" />
           <NavMenu />
         </div>
         
         <div className="hamburger-menu-cell-grid">
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-12" route="Spheres/shape" text="Forma" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-13" route="Spheres/doubt" text="Dubbio" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-14" route="Spheres/spaces" text="Spazi" />
+          <div className="hamburger-menu-cell" />
+          <ViewSelector className="hamburger-menu-cell" route="Spheres/shape" text="Forma" />
+          <ViewSelector className="hamburger-menu-cell" route="Spheres/doubt" text="Dubbio" />
+          <ViewSelector className="hamburger-menu-cell" route="Spheres/spaces" text="Spazi" />
 
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-21" route="Phenomena/intro" text="Fenomeno" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-22" route="Phenomena/shapeAnalysis" text="Forma" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-23" route="Phenomena/doubtAnalysis" text="Dubbio" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-24" route="Phenomena/spaceAnalysis" text="Spazio" />
+          <ViewSelector className="hamburger-menu-cell" route="Phenomena/intro" text="Fenomeno" />
+          <ViewSelector className="hamburger-menu-cell" route="Phenomena/shapeAnalysis" text="Forma" />
+          <ViewSelector className="hamburger-menu-cell" route="Phenomena/doubtAnalysis" text="Dubbio" />
+          <ViewSelector className="hamburger-menu-cell" route="Phenomena/spaceAnalysis" text="Spazio" />
 
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-31" route="Process/intro" text="Processo" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-32" route="Process/transforming" text="Trasformare" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-33" route="Process/doubting" text="Dubitare" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-34" route="Process/combining" text="Combinare" />
+          <ViewSelector className="hamburger-menu-cell" route="Process/intro" text="Processo" />
+          <ViewSelector className="hamburger-menu-cell" route="Process/transforming" text="Trasformare" />
+          <ViewSelector className="hamburger-menu-cell" route="Process/doubting" text="Dubitare" />
+          <ViewSelector className="hamburger-menu-cell" route="Process/combining" text="Combinare" />
 
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-41" route="Problem/intro" text="Problema" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-42" route="Problem/realism" text="Realismo" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-43" route="Problem/cancellation" text="Cancellazione" />
-          <ViewSelector className="hamburger-menu-cell hamburger-menu-cell-44" route="Problem/plot" text="Trama" />    
+          <ViewSelector className="hamburger-menu-cell" route="Problem/intro" text="Problema" />
+          <ViewSelector className="hamburger-menu-cell" route="Problem/realism" text="Realismo" />
+          <ViewSelector className="hamburger-menu-cell" route="Problem/cancellation" text="Cancellazione" />
+          <ViewSelector className="hamburger-menu-cell" route="Problem/plot" text="Trama" />
         </div>
         
       </div>
