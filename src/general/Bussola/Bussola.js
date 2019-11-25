f51d153a2a07580744a5c00e54b10ee3a8469237
iosonosempreio <tommasoelli@Tommasos-MacBook-Pro-2.local>
2019-11-24 23:03:53 -0500

    added component Bussola

---
+++ src/general/Bussola/Bussola.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompass } from '@fortawesome/free-regular-svg-icons';
+
+class Bussola extends Component {
+  render() {
+    return <div style={this.props.style} className="more-info"><FontAwesomeIcon icon={faCompass} /></div>;
+  }
+}
+
+export default Bussola;
