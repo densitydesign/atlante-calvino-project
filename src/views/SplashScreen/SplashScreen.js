16566d052e8155f2272b97e84807ff9f3f290664
crillion <crillion@tiscali.it>
2019-11-29 18:37:02 +0100

    splash screen : work in progress for the animation.

--- src/views/SplashScreen/SplashScreen.js
+++ src/views/SplashScreen/SplashScreen.js
@@ -1,18 +1,48 @@
 
 import React from 'react';
-import { Link } from 'react-router-dom';
-import * as d3 from 'd3';
+import V from './logo.js';
+
+import './SplashScreen.css';
 
 export default class SplashScreen extends React.Component
 {
+  componentDidMount()
+  {
+    this._chart = V.initialize(
+      this._rootNode,
+      this.props.data,
+      this.props.originalData);
+  }
+
+  componentDidUpdate()
+  {
+    V.update(null);
+  }
+
+  componentWillUnmount()
+  {
+    V.destroy(this._rootNode);
+  }
+
+  _setRef(componentNode)
+  {
+    this._rootNode = componentNode;
+  }
+
   render()
   {
+    return <div>pippo</div>;
+/*    
     return (
-      <Link to="/AtlasIntro">
-        <div>
-          <h2>SplashScreen</h2>
-        </div>
-      </Link>
+      <section class="container-fluid">
+        <section id="interactive-logo" class="container">
+          <div class="row d-flex align-items-center">
+            <div id="logo-box" class="col-12 col-sm-12 col-md-12 col-lg-10 mx-auto d-flex flex-column justify-content-center">
+            </div>
+          </div>
+        </section>
+      </section>
     );
+*/
   }
 }
---
+++ src/views/SplashScreen/logo.js
@@ -0,0 +1,230 @@
+
+import * as d3 from 'd3';
+
+const V = {};
+const svgUrl = "./assets/logo-big-polygons-01.svg";
+
+d3.xml(svgUrl)
+    .mimeType("image/svg+xml")
+    .get(function(error, xml) {
+        if (error) throw error;
+        document.getElementById('logo-box')
+            .appendChild(xml.documentElement);
+
+        d3.select('#logo-box')
+            .append('h2')
+            .attr('class', 'text-center')
+            .style('opacity', 0)
+            .style('letter-spacing', '-0.5px')
+            .html(`
+                <span lang="it-IT">Letteratura e visualizzazione</span>
+                <span lang="fr-FR">Littérature et visualisation</span>
+                <span lang="en-UK">Literature and Visualization</span>
+                `)
+
+        let calogo = d3.select("#calogo");
+
+        let width = calogo.attr("viewBox")
+            .split(' ')[2],
+            height = calogo.attr("viewBox")
+            .split(' ')[3],
+            rootNode,
+            nodes = [],
+            node = calogo.selectAll(".node"),
+            scaleFactor = 1,
+            lettersPolygons = [],
+            margin = {};
+
+        let config = [{
+            collisionMargin: .25,
+            amountNodes: 4000,
+            radiusRootNode: 6,
+            rootPosition: [201.0764923095703 + 3 + 0.002, 88.30018615722656 + 3 - 0.002],
+            margin: {
+                top: 0,
+                bottom: scaleFactor * height,
+                left: 0,
+                right: scaleFactor * width
+            }
+    }]
+
+        let configuration = config[0];
+
+        // A class for converting the poligons into an object suitable for the library svg-points.js
+        class polygonModel {
+            constructor(type, attr, element) {
+                this['type'] = type;
+                this[attr] = d3.select(element)
+                    .attr(attr);
+            }
+        }
+
+        // Convert all polygons in array of coordinates, thanks to svg-points.js
+        calogo.selectAll('polygon')
+            .each(function() {
+                let thisPolygon = new polygonModel('polygon', 'points', this);
+                let thisPolygonPointsObjects = SVGPoints.toPoints(thisPolygon);
+                let thisPolygonPointsArray = [];
+                thisPolygonPointsObjects.forEach(function(d) {
+                    thisPolygonPointsArray.push([d.x, d.y]);
+                });
+                lettersPolygons.push(thisPolygonPointsArray);
+            });
+
+        // This function checks whether the circle is or not whitin any of the letters polygon
+        function checkIfInside(coordinates) {
+            let flag = 0;
+            lettersPolygons.forEach(function(d) {
+                if (d3.polygonContains(d, coordinates)) {
+                    flag++;
+                }
+            })
+            // if the flag is grater than one, the point is inside a letter eye
+            if (flag == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // A function for randomly adding circles to the nodes array
+        function addNodes(n) {
+            for (let i = 0; i < n; i++) {
+
+                let myX = d3.randomUniform(configuration.margin.left, configuration.margin.right)();
+                let myY = d3.randomUniform(configuration.margin.top, configuration.margin.bottom)();
+                let myRadius = Math.round(fibonacci[Math.round(d3.randomUniform(1, 4)())]) / 3.1;
+
+                nodes.push({ 'id': i, 'r': myRadius, 'x': myX, 'y': myY });
+            }
+            if (configuration.radiusRootNode && !rootNode) {
+                rootNode = nodes[0];
+                rootNode.fx = configuration.rootPosition[0];
+                rootNode.fy = configuration.rootPosition[1];
+                rootNode.r = configuration.radiusRootNode;
+            }
+        }
+
+        let collideForce = d3.forceCollide(function(d) { return d.r + configuration.collisionMargin });
+
+        function ticked() {
+            node.attr("cx", function(d) { return d.x = Math.max(configuration.margin.left + d.r, Math.min(configuration.margin.right - d.r, d.x)); })
+                .attr("cy", function(d) { return d.y = Math.max(configuration.margin.top + d.r, Math.min(configuration.margin.bottom - d.r, d.y)); })
+                .classed('is-inside', function(d) { return checkIfInside([d.x, d.y]) })
+        }
+
+        //Declare simulation
+        let simulation = d3.forceSimulation(nodes)
+            .force("x", null)
+            .force("y", null)
+            .force("collide", d3.forceCollide(function(d) { return d.r + configuration.collisionMargin }))
+            .alpha(1)
+            .alphaMin(.5)
+            .alphaDecay(0.01)
+            .on("tick", ticked);
+
+        // Update function
+        function update() {
+
+            function drawGraph() {
+                // Apply the general update pattern to the nodes.
+                node = node.data(nodes, function(d) { return d.id; });
+
+                node.exit()
+                    .remove();
+
+                node = node.enter()
+                    .append("circle")
+                    .classed('node', true)
+                    .classed('is-inside', function(d) { return checkIfInside([d.x, d.y]) })
+                    .classed('root-node', function(d) { if (d.id == 0) { return true } else { return false } })
+                    .attr('cx', function(d) { return d.x; })
+                    .attr('cy', function(d) { return d.y; })
+                    .attr("r", 0)
+                    .merge(node);
+
+                node.transition()
+                    .delay(function(d, i) { return i+50; })
+                    .duration(1500)
+                    .ease(d3.easeElasticOut)
+                    .attr('r', function(d) { return d.r; });
+
+                d3.select('#logo-box h2')
+                    .transition()
+                    .delay(500)
+                    .duration(1500)
+                    .style('opacity', 1)
+                    .style('letter-spacing', '.5px')
+
+            }
+
+            drawGraph();
+
+            // Update and restart the simulation.
+            simulation.nodes(nodes)
+                .alpha(1)
+                .restart();
+        }
+
+        // Anticolllision on mouse move
+        calogo.on("mousemove", function() {
+            let p1 = d3.mouse(this);
+            if (rootNode) {
+                rootNode.fx = p1[0];
+                rootNode.fy = p1[1];
+                simulation
+                    .alpha(1)
+                    .restart(); //reheat the simulation
+            }
+        });
+
+        // Anticolllision on touch
+        calogo.on("touchmove", function() {
+            let p1 = d3.touches(this)[0];
+            d3.event.preventDefault();
+            if (rootNode) {
+                rootNode.fx = p1[0];
+                rootNode.fy = p1[1];
+                simulation
+                    .alpha(1)
+                    .restart(); //reheat the simulation
+            }
+        });
+
+        calogo.on("mouseleave", function() {
+            d3.select('.root-node')
+                .transition()
+                .duration(0)
+                .on("end", function() {
+                    rootNode.fx = configuration.rootPosition[0];
+                    rootNode.fy = configuration.rootPosition[1];
+                });
+        });
+
+        calogo.on("touchend", function() {
+            d3.select('.root-node')
+                .transition()
+                .duration(0)
+                .on("end", function() {
+                    rootNode.fx = configuration.rootPosition[0];
+                    rootNode.fy = configuration.rootPosition[1];
+                });
+        });
+
+        d3.json('./assets/bubbles.json', function(err, data) {
+            nodes = data;
+
+            let maxRadius = d3.max(nodes, function(d) { return d.r })
+            rootNode = nodes.filter(function(d) {
+                return d.r == maxRadius;
+            })[0]
+
+            simulation
+                .nodes(nodes)
+                .force("x", d3.forceX(function(d) { return d.x }))
+                .force("y", d3.forceY(function(d) { return d.y }))
+
+            update();
+        })
+
+    }); // end of everything
