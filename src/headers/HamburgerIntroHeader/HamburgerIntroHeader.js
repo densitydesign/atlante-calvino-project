fed547714650a70793d5eacb7a6f7a2d64239ec6
crillion <crillion@tiscali.it>
2019-12-09 14:15:22 +0100

    compass : added text content to the panels; HamburgerIntroHeader : made hamburger 1 unit wide

--- src/headers/HamburgerIntroHeader/HamburgerIntroHeader.js
+++ src/headers/HamburgerIntroHeader/HamburgerIntroHeader.js
@@ -9,8 +9,8 @@ export default class HamburgerIntroHeader extends React.Component
   {
     return (
       <div className="top-nav navigations">
-        <MainMenu style={{gridColumn: 'span 2'}} />
-        <div style={{gridColumn: 'span 20'}} />
+        <MainMenu style={{gridColumn: 'span 1'}} />
+        <div style={{gridColumn: 'span 21'}} />
         <IntroLink style={{gridColumn: 'span 2'}} />
       </div>
     );
--- src/views/Compass/Compass.js
+++ src/views/Compass/Compass.js
@@ -19,9 +19,18 @@ export default class Compass extends React.Component
 
           <div id="compass">Bussola</div>
           <div>
-          <SlidingPanel open="true">Lische</SlidingPanel>
-          <SlidingPanel open="false">Sinuosa</SlidingPanel>
-          <SlidingPanel open="false">Reti</SlidingPanel>
+            <SlidingPanel open="true">
+              Lische<br />
+              Zwei flinke Boxer jagen die quirlige Eva und ihren Mops durch Sylt. Franz jagt im komplett verwahrlosten Taxi quer durch Bayern. Zwölf Boxkämpfer jagen Viktor quer über den großen Sylter Deich. Vogel Quax zwickt Johnys Pferd Bim. Sylvia wagt quick den Jux bei Pforzheim. Polyfon zwitschernd aßen
+            </SlidingPanel>
+            <SlidingPanel open="false">
+              Sinuosa<br />
+              Vogel Quax zwickt Johnys Pferd Bim. Sylvia wagt quick den Jux bei Pforzheim. Polyfon zwitschernd aßen Mäxchens Vögel Rüben, Joghurt und Quark. „Fix, Schwyz!“ quäkt Jürgen blöd vom Paß. Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich. Falsches Üben von Xylophonmusik quält jeden größeren Zwerg. Heizölrückstoßabdämpfung.Zwei flinke Boxer jagen die quirlige Eva und ihren Mops durch Sylt. Franz jagt im komplett verwahrlosten Taxi quer durch Bayern. Zwölf Boxkämpfer jagen Viktor quer über den großen Sylter Deich. 
+            </SlidingPanel>
+            <SlidingPanel open="false">
+              Reti<br />
+              Seit 1975 fehlen in den meisten Testtexten die Zahlen, weswegen nach TypoGb. 204 § ab dem Jahr 2034 Zahlen in 86 der Texte zur Pflicht werden. Nichteinhaltung wird mit bis zu 245 € oder 368 $ bestraft. Genauso wichtig in sind mittlerweile auch Âçcèñtë, die in neueren Schriften aber fast immer enthalten sind. Ein wichtiges aber schwierig zu integrierendes Feld sind OpenType-Funktionalitäten. Je nach Software und Voreinstellungen können eingebaute Kapitälchen, Kerning oder Ligaturen (sehr pfiffig) nicht richtig dargestellt werden.Dies ist ein Typoblindtext. An ihm kann man sehen, ob alle Buchstaben da sind und wie sie aussehen. 
+            </SlidingPanel>
           </div>
 {/*          
           <div id="fishbones" ref={ fishbones => this.fishbones = fishbones }>Lische</div>
