da637ee2610a9ea1423514f1c89056e69f1e0e98
crillion <crillion@tiscali.it>
2019-12-12 14:16:18 +0100

    problem intro : fixed header type

--- src/views/ProblemIntro/ProblemIntro.js
+++ src/views/ProblemIntro/ProblemIntro.js
@@ -17,7 +17,7 @@ export default class ProblemIntro extends React.Component
           <div className="category-intro-block-row">
             <div className="category-intro-main-block">
               <small className="current-phase-subheader"><strong>FASE 3</strong></small>
-              <h2>IL PROBLEMA</h2>
+              <h1>IL PROBLEMA</h1>
               <p>Il problema del realismo accompagna tutti i narratori moderni, Calvino incluso, che si è formato in un'epoca di neo-realismo e poi
                 se ne è allontanato anni-luce. Ma senza mai riuscire a superare il tormento che lega la descrizione del mondo alla costruzione di una trama.
                 Trama che per Calvino tende spesso a prendere una forma modulare piuttosto che progressiva: legata all'incremento potenzialmente infinito della
