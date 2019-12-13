cc1e5fc7ee6ac907278e206248696f6c310e4c08
crillion <crillion@tiscali.it>
2019-12-13 12:29:01 +0100

    compass : now multiple panels stay open, based on order, as per design

--- src/general/SlidingPanel/SlidingPanel.js
+++ src/general/SlidingPanel/SlidingPanel.js
@@ -9,7 +9,7 @@ export default class SlidingPanel extends React.Component
   {
     super(props);
 
-    this.state = { open : JSON.parse(props.open) };
+//    this.state = { open : JSON.parse(props.open) };
     this.setWrapperRef = this.setWrapperRef.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
@@ -33,22 +33,25 @@ export default class SlidingPanel extends React.Component
   {
     if(!this.wrapperRef) return;
 
-    this.setState({ open : this.wrapperRef.contains(event.target) });
+    if(this.wrapperRef.contains(event.target)) this.props.panelClicked(this.props.id);
 
-console.log("panel id : ", this.props.id);
+//    this.setState({ open : this.wrapperRef.contains(event.target) });
+
+//console.log("panel id : ", this.props.id);
 
 //    this.setState({ open : this.wrapperRef.contains(event.target) || this.props.getSelectedPanel() > Number.parseInt(this.props.id)});
   }
 
   render()
   {
+console.log("render panel");
     return (
       <div 
-        className={"sliding-panel " + (this.state.open ? "sliding-panel-open" : "sliding-panel-closed")} 
+        className={"sliding-panel " + (this.props.open ? "sliding-panel-open" : "sliding-panel-closed")} 
         ref={this.setWrapperRef} 
         style={{ 
           background : this.props.background,
-          right : Number.parseInt(this.props.right) + (this.state.open ? 400 : 0),
+          right : Number.parseInt(this.props.right) + (this.props.open ? 400 : 0),
           zIndex : this.props.zIndex
         }}>
 
--- src/views/Compass/Compass.js
+++ src/views/Compass/Compass.js
@@ -14,11 +14,12 @@ export default class Compass extends React.Component
 
     this.getSelectedPanel = this.getSelectedPanel.bind(this);
     this.setSelectedPanel = this.setSelectedPanel.bind(this);
+    this.panelClicked = this.panelClicked.bind(this);
   }
 
   componentDidUpdate()
   {
-    this.refs.fishbones.getDOMNode().focus();    
+//    this.refs.fishbones.getDOMNode().focus();    
   }
 
   getSelectedPanel()
@@ -31,8 +32,15 @@ export default class Compass extends React.Component
     this.state.selectedPanel = selectedPanel;
   }
 
+  panelClicked(panelId)
+  {
+    this.setState({ selectedPanel : panelId });
+console.log("panelClicked : ", panelId);    
+  }
+
   render()
   {
+console.log("render compass");    
     return (
       <div>
         <HamburgerIntroHeader />
@@ -41,9 +49,9 @@ export default class Compass extends React.Component
             <h1>Bussola</h1>
           </div>
           <div>
-            <SlidingPanel id="1" background="lightgray" open="true" zIndex="1" right="-180" title="Lische" getSelectedPanel={this.getSelectedPanel} setSelectedPanel={this.setSelectedPanel} text="Zwei flinke Boxer jagen die quirlige Eva und ihren Mops durch Sylt. Franz jagt im komplett verwahrlosten Taxi quer durch Bayern. Zwölf Boxkämpfer jagen Viktor quer über den großen Sylter Deich. Vogel Quax zwickt Johnys Pferd Bim. Sylvia wagt quick den Jux bei Pforzheim. Polyfon zwitschernd aßen" />
-            <SlidingPanel id="2" background="yellow" open="false" zIndex="2" right="-260" title="Sinuosa" getSelectedPanel={this.getSelectedPanel} setSelectedPanel={this.setSelectedPanel} text="Vogel Quax zwickt Johnys Pferd Bim. Sylvia wagt quick den Jux bei Pforzheim. Polyfon zwitschernd aßen Mäxchens Vögel Rüben, Joghurt und Quark. „Fix, Schwyz!“ quäkt Jürgen blöd vom Paß. Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich. Falsches Üben von Xylophonmusik quält jeden größeren Zwerg. Heizölrückstoßabdämpfung.Zwei flinke Boxer jagen die quirlige Eva und ihren Mops durch Sylt. Franz jagt im komplett verwahrlosten Taxi quer durch Bayern. Zwölf Boxkämpfer jagen Viktor quer über den großen Sylter Deich." />
-            <SlidingPanel id="3" background="lightgreen" open="false" zIndex="3" right="-340%" title="Reti" getSelectedPanel={this.getSelectedPanel} setSelectedPanel={this.setSelectedPanel} text="Seit 1975 fehlen in den meisten Testtexten die Zahlen, weswegen nach TypoGb. 204 § ab dem Jahr 2034 Zahlen in 86 der Texte zur Pflicht werden. Nichteinhaltung wird mit bis zu 245 € oder 368 $ bestraft. Genauso wichtig in sind mittlerweile auch Âçcèñtë, die in neueren Schriften aber fast immer enthalten sind. Ein wichtiges aber schwierig zu integrierendes Feld sind OpenType-Funktionalitäten. Je nach Software und Voreinstellungen können eingebaute Kapitälchen, Kerning oder Ligaturen (sehr pfiffig) nicht richtig dargestellt werden.Dies ist ein Typoblindtext. An ihm kann man sehen, ob alle Buchstaben da sind und wie sie aussehen." />
+            <SlidingPanel id="1" background="lightgray" open={this.state.selectedPanel >= 1} zIndex="1" right="-180" title="Lische" getSelectedPanel={this.getSelectedPanel} setSelectedPanel={this.setSelectedPanel} panelClicked={this.panelClicked} text="Zwei flinke Boxer jagen die quirlige Eva und ihren Mops durch Sylt. Franz jagt im komplett verwahrlosten Taxi quer durch Bayern. Zwölf Boxkämpfer jagen Viktor quer über den großen Sylter Deich. Vogel Quax zwickt Johnys Pferd Bim. Sylvia wagt quick den Jux bei Pforzheim. Polyfon zwitschernd aßen" />
+            <SlidingPanel id="2" background="yellow" open={this.state.selectedPanel >= 2} zIndex="2" right="-260" title="Sinuosa" getSelectedPanel={this.getSelectedPanel} setSelectedPanel={this.setSelectedPanel} panelClicked={this.panelClicked} text="Vogel Quax zwickt Johnys Pferd Bim. Sylvia wagt quick den Jux bei Pforzheim. Polyfon zwitschernd aßen Mäxchens Vögel Rüben, Joghurt und Quark. „Fix, Schwyz!“ quäkt Jürgen blöd vom Paß. Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich. Falsches Üben von Xylophonmusik quält jeden größeren Zwerg. Heizölrückstoßabdämpfung.Zwei flinke Boxer jagen die quirlige Eva und ihren Mops durch Sylt. Franz jagt im komplett verwahrlosten Taxi quer durch Bayern. Zwölf Boxkämpfer jagen Viktor quer über den großen Sylter Deich." />
+            <SlidingPanel id="3" background="lightgreen" open={this.state.selectedPanel >= 3} zIndex="3" right="-340%" title="Reti" getSelectedPanel={this.getSelectedPanel} setSelectedPanel={this.setSelectedPanel} panelClicked={this.panelClicked} text="Seit 1975 fehlen in den meisten Testtexten die Zahlen, weswegen nach TypoGb. 204 § ab dem Jahr 2034 Zahlen in 86 der Texte zur Pflicht werden. Nichteinhaltung wird mit bis zu 245 € oder 368 $ bestraft. Genauso wichtig in sind mittlerweile auch Âçcèñtë, die in neueren Schriften aber fast immer enthalten sind. Ein wichtiges aber schwierig zu integrierendes Feld sind OpenType-Funktionalitäten. Je nach Software und Voreinstellungen können eingebaute Kapitälchen, Kerning oder Ligaturen (sehr pfiffig) nicht richtig dargestellt werden.Dies ist ein Typoblindtext. An ihm kann man sehen, ob alle Buchstaben da sind und wie sie aussehen." />
           </div>
 {/*          
           <div>
