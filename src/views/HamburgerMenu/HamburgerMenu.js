5d1c69461f22c51e40c190c78fd98d0fa8ceda48
iosonosempreio <tommasoelli@Tommasos-MacBook-Pro-2.local>
2019-12-13 12:53:24 -0500

    added element to link

--- src/views/HamburgerMenu/HamburgerMenu.js
+++ src/views/HamburgerMenu/HamburgerMenu.js
@@ -13,36 +13,36 @@ export default class HamburgerMenu extends React.Component
   {
     return (
       <div className="hamburger-menu-container">
-        <HamburgerMenuHeader /> 
+        <HamburgerMenuHeader />
         <div className="hamburger-menu">
           <div className="hamburger-menu-sidebar">
             <ViewSelector className="compass-selector-cell" route="Compass" text="BUSSOLA" />
             <NavMenu />
           </div>
-          
+
           <div className="hamburger-menu-cell-grid">
 
             <div className="hamburger-menu-cell" />
-            <ViewSelector className="hamburger-menu-cell" route="Itineraries/#doubt" text="Dubbio" />
-            <ViewSelector className="hamburger-menu-cell" route="Itineraries/#spaces" text="Spazi" />
-            <ViewSelector className="hamburger-menu-cell" route="Itineraries/#shape" text="Forma" />
+            <ViewSelector className="hamburger-menu-cell" route="Itineraries/#doubt" text="Dubbio"><h3 dataClass="ciaociao"/></ViewSelector>
+            <ViewSelector className="hamburger-menu-cell" route="Itineraries/#spaces" text="Spazi"><h3/></ViewSelector>
+            <ViewSelector className="hamburger-menu-cell" route="Itineraries/#shape" text="Forma"><h3/></ViewSelector>
 
-            <ViewSelector className="hamburger-menu-cell" route="Phenomena/intro" text="Fenomeno" />
+            <ViewSelector className="hamburger-menu-cell" route="Phenomena/intro" text="Fenomeno"><h4/></ViewSelector>
             <ViewSelector className="hamburger-menu-cell" route="Phenomena/doubtAnalysis" text="Dubbio" />
             <ViewSelector className="hamburger-menu-cell" route="Phenomena/spaceAnalysis" text="Spazio" />
             <ViewSelector className="hamburger-menu-cell" route="Phenomena/shapeAnalysis" text="Forma" />
 
-            <ViewSelector className="hamburger-menu-cell" route="Process/intro" text="Processo" />            
+            <ViewSelector className="hamburger-menu-cell" route="Process/intro" text="Processo"><h4/></ViewSelector>
             <ViewSelector className="hamburger-menu-cell" route="Process/doubting" text="Dubitare" />
             <ViewSelector className="hamburger-menu-cell" route="Process/transforming" text="Trasformare" />
             <ViewSelector className="hamburger-menu-cell" route="Process/combining" text="Combinare" />
 
-            <ViewSelector className="hamburger-menu-cell" route="Problem/intro" text="Problema" />
+            <ViewSelector className="hamburger-menu-cell" route="Problem/intro" text="Problema"><h4/></ViewSelector>
             <ViewSelector className="hamburger-menu-cell" route="Problem/plot" text="Trama" />
             <ViewSelector className="hamburger-menu-cell" route="Problem/realism" text="Realismo" />
-            <ViewSelector className="hamburger-menu-cell" route="Problem/cancellation" text="Cancellazione" />            
+            <ViewSelector className="hamburger-menu-cell" route="Problem/cancellation" text="Cancellazione" />
 
-          </div>        
+          </div>
         </div>
       </div>
     );
--- src/views/ViewSelector/ViewSelector.js
+++ src/views/ViewSelector/ViewSelector.js
@@ -6,12 +6,25 @@ export default class ViewSelector extends React.Component
 {
   render()
   {
+    let children = React.Children.toArray(this.props.children)
+    let the_link = this.props.route.includes("#") ? <HashLink to={this.props.route}>{this.props.text}</HashLink> : <Link to={this.props.route}>{this.props.text}</Link>
+
+    if (children.length < 1) {
+      let obj = { type: 'span'}
+      children.push(obj)
+    }
+
     return (
       <div className={this.props.className}>
         {
-          this.props.route.includes("#") ? 
-          <HashLink to={this.props.route}>{this.props.text}</HashLink> :
-          <Link to={this.props.route}>{this.props.text}</Link>
+          children.map((d,i)=>{
+            console.log(d);
+            let this_class = ""
+            if (d.props && d.props.dataClass) {
+              this_class = d.props.dataClass
+            }
+            return <d.type key={i} className={this_class}>{the_link}</d.type>
+          })
         }
       </div>
     );
