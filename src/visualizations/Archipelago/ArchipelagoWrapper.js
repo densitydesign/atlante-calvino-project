39550fba8dbce5ed49231904b429b5bc072e4ea4
crillion <crillion@tiscali.it>
2019-12-17 16:57:45 +0100

    archipelago : added data files; stub method for dataLoad

--- src/visualizations/Archipelago/ArchipelagoWrapper.js
+++ src/visualizations/Archipelago/ArchipelagoWrapper.js
@@ -3,8 +3,13 @@ import React from 'react';
 
 import Archipelago from './Archipelago';
 
-export default class Trasformare extends React.Component
+export default class ArchipelagoWrapper extends React.Component
 {
+  loadData()
+  {
+
+  }
+
   render = () => {
     return (
       <div className="main">
