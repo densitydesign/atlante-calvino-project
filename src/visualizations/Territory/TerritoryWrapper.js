05492a9d46d2026355c308890d6830734a222fe4
crillion <crillion@tiscali.it>
2019-12-18 18:55:59 +0100

    territory : loading data from file

--- src/visualizations/Territory/TerritoryWrapper.js
+++ src/visualizations/Territory/TerritoryWrapper.js
@@ -1,13 +1,19 @@
 
 import React from 'react';
+import * as d3 from 'd3';
 
 import Territory from './Territory';
 
 export default class TerritoryWrapper extends React.Component
 {
+  componentDidMount()
+  {
+    this.loadData();
+  }
+
   loadData()
   {
-
+    d3.json(process.env.PUBLIC_URL + "/territory_graphical_data.json").then(treat_json);
   }
 
   render = () => {
@@ -17,4 +23,10 @@ export default class TerritoryWrapper extends React.Component
       </div>
     );
   }
+}
+
+async function treat_json(json)
+{
+  const json_nodes = json.nodes;
+console.log("json_nodes.length : ", json_nodes.length);
 }
