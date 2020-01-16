99efcb164928f4b683a86350ec06e77ed8a63ad1
Mauro Bianchi <bianchimro@gmail.com>
2020-01-16 01:05:57 +0100

    Combin: Base page with AltOptions component

---
+++ src/general/Options/AltOptions.js
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import findIndex from "lodash/findIndex";
+import isEqual from "lodash/isEqual";
+import indexOf from "lodash/indexOf";
+import "../../App.css";
+import "./Options.css";
+
+import Dropdown from "react-bootstrap/Dropdown";
+
+class AltOptions extends Component {
+  state = {
+    show: false,
+    selectedIndices: {}
+  };
+
+  updateStateFromValue = () => {
+    const { value, multiple, options } = this.props;
+    if (multiple) {
+      const indices = options
+        .map((item, i) => (indexOf(value, item.label) !== -1 ? i : undefined))
+        .filter(i => i !== undefined);
+      const selectedIndices = {};
+      indices.forEach(i => (selectedIndices[i] = true));
+      this.setState({ selectedIndices });
+    } else {
+      if (value) {
+        const i = findIndex(options, x => x.label === value);
+        this.setState({ selectedIndices: { [i]: true } });
+      }
+    }
+  };
+
+  componentDidMount() {
+    this.updateStateFromValue();
+  }
+
+  componentDidUpdate(oldProps) {
+    if (oldProps.value !== this.props.value) {
+      this.updateStateFromValue();
+    }
+  }
+
+  handleChange = index => {
+    const { selectedIndices } = this.state;
+    const { multiple, options, onChange } = this.props;
+    const newSelected = !selectedIndices[index];
+
+    if (!multiple) {
+      this.setState({ selectedIndices: { [index]: newSelected } });
+      onChange(newSelected ? options[index] : null);
+    } else {
+      const newSelectedIndices = {
+        ...selectedIndices,
+        [index]: newSelected
+      };
+      this.setState({ selectedIndices: newSelectedIndices });
+      const values = Object.keys(newSelectedIndices)
+        .sort()
+        .filter(x => newSelectedIndices[x])
+        .map(x => options[x]);
+      onChange(values);
+    }
+  };
+
+  toggleDropDown = (isOpen, event, metadata) => {
+    const { multiple } = this.props;
+    if (metadata.source === "select" && multiple) {
+      this.setState({
+        show: true
+      });
+    } else {
+      this.setState({
+        show: undefined
+      });
+    }
+  };
+
+  render() {
+    const { selectedIndices } = this.state;
+    const { multiple, title, value, style, options=[] } = this.props;
+    const indices = Object.keys(selectedIndices)
+        .map(k => selectedIndices[k] ? k : undefined)
+        .filter(i => i !== undefined);
+    const anySelected = !!indices.length
+    const current = multiple || !anySelected ? undefined : options[indices[0]].label
+
+    return (
+      <div className="options-container" style={style}>
+        <Dropdown onToggle={this.toggleDropDown} show={this.state.show}>
+          <Dropdown.Toggle>
+            {!multiple && anySelected && (
+              <div>
+                <span className="micro-title">{title}</span>
+                <span className="current-selection">
+                  {current}
+                </span>
+              </div>
+            )}
+            {!multiple && !anySelected && title}
+            {multiple && title}
+          </Dropdown.Toggle>
+          <Dropdown.Menu
+            onToggle={multiple ? undefined : this.toggleDropDown}
+            show={this.state.show}
+          >
+            {this.props.options.map((d, i) => {
+              return (
+                <Dropdown.Item
+                  key={i}
+                  onClick={() => this.handleChange(i)}
+                  className={{ active: selectedIndices[i] }}
+                >
+                  {d.label}
+                </Dropdown.Item>
+              );
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    );
+  }
+}
+
+export default AltOptions;
+
+AltOptions.defaultProps = {
+  style: {
+    gridColumn: "span 4"
+  },
+  options: [],
+  multiple: false,
+  title: "Options",
+  value: null,
+  onChange: value => {}
+};
--- src/visualizations/Combine/Combine.js
+++ src/visualizations/Combine/Combine.js
@@ -1,8 +1,100 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import MainMenu from "../../general/MainMenu";
+import PageTitle from "../../general/PageTitle";
+import MoreInfo from "../../general/MoreInfo";
+import Bussola from "../../general/Bussola";
+import Search from '../../general/Search';
+import Loading from "../../general/Loading";
+
+import AltOptions  from "../../general/Options/AltOptions";
+
+
+const tipologiaOptions = [
+    { label : "tutti" },
+    { label : "uno" },
+    { label : "due" },
+    { label : "tre" },
+    { label : "quattro" },
+    { label : "cinque" },
+]
+
+const dettaglioOptions = [
+    { label : "ambito" },
+    { label : "categorie" },
+]
+
+const aggregazioneOptions = [
+    { label : "aggregato" },
+    { label : "non aggregato" },
+]
+
+const cercaOptions = [
+  { label : "titolo" },
+  { label : "boh" },
+]
+
+
 
 class Combine extends Component {
+  state = {
+    isLoading: false
+  };
+
   render() {
-    return <div>Combine</div>;
+    return (
+      <div className="trasformare main">
+        <div className="top-nav navigations">
+          <MainMenu className="main-menu" style={{ gridColumn: "span 1" }} />
+          <PageTitle title={"Hello combine :)"} style={{ gridColumn: "span 10" }} />
+
+          {this.state.isLoading && <Loading style={{ gridColumn: "span 3" }} />}
+          {!this.state.isLoading && (
+            <AltOptions
+              title="Cerca per"
+              options={cercaOptions}
+              value={'titolo'}
+              style={{ gridColumn: "span 3" }}
+            />
+          )}
+
+          {this.state.isLoading && <Loading style={{ gridColumn: "span 8" }} />}
+          {!this.state.isLoading && (
+            <Search
+              style={{ gridColumn: "span 8" }}
+              data={{options:[]}}
+              changeOptions={this.changeRicerca}
+            />
+          )}
+          <MoreInfo style={{ gridColumn: "span 1" }} />
+          <Bussola style={{ gridColumn: "span 1" }} />
+        </div>
+
+        <div className="bottom-nav navigations">
+          <AltOptions
+            title="Tipologia"
+            multiple
+            options={tipologiaOptions}
+            style={{ gridColumn: "span 8", textAlign: "center" }}
+            onChange={(tipologia) => { console.log("tipologiaChanged", tipologia) }}
+            
+          />
+
+          <AltOptions
+            title="Dettaglio"
+            options={dettaglioOptions}
+            style={{ gridColumn: "span 8", textAlign: "center" }}
+            
+          />
+
+          <AltOptions
+            title="Aggregazioni"
+            options={aggregazioneOptions}
+            style={{ gridColumn: "span 8", textAlign: "center" }}
+            
+          />
+        </div>
+      </div>
+    );
   }
 }
 
