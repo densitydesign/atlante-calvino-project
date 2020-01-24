f707d965bebf3ad005e4720de6bf06c84b641994
Beatrice Gobbo <gobbo.beatrice92@gmail.com>
2020-01-24 12:56:30 +0100

    fixed div

--- src/panels/TerritorySpaceMainOptionsSubPanel/TerritorySpaceMainOptionsSubPanel.js
+++ src/panels/TerritorySpaceMainOptionsSubPanel/TerritorySpaceMainOptionsSubPanel.js
@@ -13,7 +13,6 @@ export default class TerritorySpaceMainOptionsSubPanel extends React.Component
                 <div className="categories-panel"><h4>TERRESTRI</h4><h4>NON TERRESTRI</h4><h4>ALTRO</h4></div>
 
       <div className="territory-space-main-options-subpanel">
-      <h4>TERRESTRI</h4
         <RadioButton
           id={this.props.genericNonTerrestrialRadioButtonId}
           caption={this.props.genericNonTerrestrialRadioButtonCaption}
