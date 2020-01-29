0e98a82655d0fe09e3c237a66abc60243b049e10
bea92 <gobbo.beatrice92@gmail.com>
2020-01-29 12:15:18 +0000

    edit scheda but contains errors

--- src/informationSheets/DoubtInformationSheet/DoubtInformationSheet.js
+++ src/informationSheets/DoubtInformationSheet/DoubtInformationSheet.js
@@ -2,6 +2,7 @@
 import React from 'react';
 
 import HamburgerCompassHeader from '../../headers/HamburgerCompassHeader/HamburgerCompassHeader';
+import DoubtTypePerYearWrapper from '../../visualizations/DoubtTypePerYear/DoubtTypePerYearWrapper';
 
 import './DoubtInformationSheet.css';
 
@@ -14,10 +15,11 @@ export default class DoubtInformationSheet extends React.Component
         <HamburgerCompassHeader />
 
         <h1>SCHEDA DUBBIO</h1>
+        <DoubtTypePerYearWrapper />
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur dictum sapien sed sollicitudin. Nunc eu rhoncus tortor, vitae vulputate dolor. Fusce interdum tortor id augue ullamcorper consequat a sed quam. In interdum elementum purus, at dapibus justo mollis rhoncus. Etiam rhoncus nec nulla a vulputate. Etiam tincidunt consectetur metus in condimentum. Pellentesque sapien lectus, consectetur eu risus non, rutrum pellentesque arcu. Curabitur eros magna, mattis sit amet nisi ut, lobortis pretium nibh. Morbi malesuada diam quis nunc aliquam, ac consequat augue cursus. Quisque eu velit eget lacus gravida pellentesque. Nam viverra laoreet felis at lacinia. Quisque cursus imperdiet elit, ultricies consectetur neque rutrum et. Nam mattis dui nulla, quis pretium massa suscipit vel.           
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur dictum sapien sed sollicitudin. Nunc eu rhoncus tortor, vitae vulputate dolor. Fusce interdum tortor id augue ullamcorper consequat a sed quam. In interdum elementum purus, at dapibus justo mollis rhoncus. Etiam rhoncus nec nulla a vulputate. Etiam tincidunt consectetur metus in condimentum. Pellentesque sapien lectus, consectetur eu risus non, rutrum pellentesque arcu. Curabitur eros magna, mattis sit amet nisi ut, lobortis pretium nibh. Morbi malesuada diam quis nunc aliquam, ac consequat augue cursus. Quisque eu velit eget lacus gravida pellentesque. Nam viverra laoreet felis at lacinia. Quisque cursus imperdiet elit, ultricies consectetur neque rutrum et. Nam mattis dui nulla, quis pretium massa suscipit vel.
         </p>
-      </>      
+      </>
     );
   }
 }
---
+++ src/visualizations/DoubtTypePerYear/DoubtTypePerYear.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+
+import V from './DoubtTypePerYear.d3';
+
+export default class DoubtTypePerYear extends React.Component
+{
+  componentDidMount()
+  {
+    V.initialize(this._rootNode, this.props.data);
+  }
+
+  componentWillUnmount()
+  {
+    V.destroy(this._rootNode);
+  }
+
+  _setRef = componentNode => this._rootNode = componentNode;
+
+  render()
+  {
+    const style = {
+      width: '100%',
+      height: '600px',
+      marginTop: '2em',
+      marginBottom: '4em'
+    }
+    return <svg id={this.props.id} style={style} ref={this._setRef}></svg>;
+  }
+}
