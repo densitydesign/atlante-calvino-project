ab03f531ec2418d240e72333e45498cc7ee01609
bea92 <gobbo.beatrice92@gmail.com>
2020-01-29 21:16:58 +0000

    edit scheda dubbio layout

--- src/informationSheets/DoubtInformationSheet/DoubtInformationSheet.js
+++ src/informationSheets/DoubtInformationSheet/DoubtInformationSheet.js
@@ -3,6 +3,8 @@ import React from 'react';
 
 import HamburgerCompassHeader from '../../headers/HamburgerCompassHeader/HamburgerCompassHeader';
 import DoubtTypePerYearWrapper from '../../visualizations/DoubtTypePerYear/DoubtTypePerYearWrapper';
+import ArrowButton from '../../general/ArrowButton/ArrowButton';
+import { Link } from 'react-router-dom';
 
 import './DoubtInformationSheet.css';
 
@@ -18,24 +20,42 @@ export default class DoubtInformationSheet extends React.Component
             <h1>SCHEDA DUBBIO</h1>
             <h2>Occorrenze campo semantico del dubbio</h2>
             <div className="viz--info">
-              <div id="label"><p><i>Scopri i valori</i></p></div>
               <div id="legend"><p><img src={process.env.PUBLIC_URL + "/informationSheets/Scheda_Dubbio_dimensione.svg"} width="650"/></p></div>
             </div>
           </div>
           <DoubtTypePerYearWrapper  />
           <article>
-          <p>
+          <p className="intro">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur dictum sapien sed sollicitudin. Nunc eu rhoncus tortor, vitae vulputate dolor. Fusce interdum tortor id augue ullamcorper consequat a sed quam. In interdum elementum purus, at dapibus justo mollis rhoncus. Etiam rhoncus nec nulla a vulputate. Etiam tincidunt consectetur metus in condimentum. Pellentesque sapien lectus, consectetur eu risus non, rutrum pellentesque arcu. Curabitur eros magna, mattis sit amet nisi ut, lobortis pretium nibh. Morbi malesuada diam quis nunc aliquam, ac consequat augue cursus. Quisque eu velit eget lacus gravida pellentesque. Nam viverra laoreet felis at lacinia. Quisque cursus imperdiet elit, ultricies consectetur neque rutrum et. Nam mattis dui nulla, quis pretium massa suscipit vel.
           </p>
-          </article>
-          <article>
+</article>
+<article className="focus">
+          <div>
           <h3>Occorrenze campo semantico del dubbio</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur dictum sapien sed sollicitudin. Nunc eu rhoncus tortor, vitae vulputate dolor. Fusce interdum tortor id augue ullamcorper consequat a sed quam. In interdum elementum purus, at dapibus justo mollis rhoncus. Etiam rhoncus nec nulla a vulputate. Etiam tincidunt consectetur metus in condimentum. Pellentesque sapien lectus, consectetur eu risus non, rutrum pellentesque arcu. Curabitur eros magna, mattis sit amet nisi ut, lobortis pretium nibh. Morbi malesuada diam quis nunc aliquam, ac consequat augue cursus. Quisque eu velit eget lacus gravida pellentesque. Nam viverra laoreet felis at lacinia. Quisque cursus imperdiet elit, ultricies consectetur neque rutrum et. Nam mattis dui nulla, quis pretium massa suscipit vel.
           </p>
-          <img className="col-md-6" />
-          </article>
-                    </main>
+          <img className="viz--pic" />
+          </div>
+</article>
+<article className="focus-2">
+          <div>
+          <h3>Occorrenze campo semantico del dubbio</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur dictum sapien sed sollicitudin. Nunc eu rhoncus tortor, vitae vulputate dolor. Fusce interdum tortor id augue ullamcorper consequat a sed quam. In interdum elementum purus, at dapibus justo mollis rhoncus. Etiam rhoncus nec nulla a vulputate. Etiam tincidunt consectetur metus in condimentum. Pellentesque sapien lectus, consectetur eu risus non, rutrum pellentesque arcu. Curabitur eros magna, mattis sit amet nisi ut, lobortis pretium nibh. Morbi malesuada diam quis nunc aliquam, ac consequat augue cursus. Quisque eu velit eget lacus gravida pellentesque. Nam viverra laoreet felis at lacinia. Quisque cursus imperdiet elit, ultricies consectetur neque rutrum et. Nam mattis dui nulla, quis pretium massa suscipit vel.
+          </p>
+          <img className="viz--pic" />
+          </div>
+
+          <div className="sheet--info">
+            <ArrowButton arrowDirection="left" textAlign="right" text="ANALISI" route="/Phenomena/doubtAnalysis" />
+            <ArrowButton arrowDirection="right" textAlign="left" text="TAPPA 2" route="/Process/intro" />
+            <ArrowButton arrowDirection="right" textAlign="left" text="TAPPA 3" route="/Problem/intro" />
+          </div>
+</article>
+
+</main>
+
       </>
     );
   }
--- src/informationSheets/ShapeInformationSheet/ShapeInformationSheet.js
+++ src/informationSheets/ShapeInformationSheet/ShapeInformationSheet.js
@@ -26,7 +26,7 @@ export default class ShapeInformationSheet extends React.Component
           </div>
           <ListTypesPerTextWrapper  />
           <article>
-          <p>
+          <p className="intro">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur dictum sapien sed sollicitudin. Nunc eu rhoncus tortor, vitae vulputate dolor. Fusce interdum tortor id augue ullamcorper consequat a sed quam. In interdum elementum purus, at dapibus justo mollis rhoncus. Etiam rhoncus nec nulla a vulputate. Etiam tincidunt consectetur metus in condimentum. Pellentesque sapien lectus, consectetur eu risus non, rutrum pellentesque arcu. Curabitur eros magna, mattis sit amet nisi ut, lobortis pretium nibh. Morbi malesuada diam quis nunc aliquam, ac consequat augue cursus. Quisque eu velit eget lacus gravida pellentesque. Nam viverra laoreet felis at lacinia. Quisque cursus imperdiet elit, ultricies consectetur neque rutrum et. Nam mattis dui nulla, quis pretium massa suscipit vel.
           </p>
           </article>
