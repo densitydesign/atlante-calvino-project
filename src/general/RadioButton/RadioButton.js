9f1f060d10f60e09fc67b5dc9655282462e6e1a2
crillion <crillion@tiscali.it>
2020-02-04 14:27:49 +0100

    territory / dropdown : improvements in aesthetics

--- src/general/RadioButton/RadioButton.js
+++ src/general/RadioButton/RadioButton.js
@@ -7,17 +7,15 @@ export default class RadioButton extends React.Component
 {
   render()
   {
-    const buttonDownClass = this.props.buttonDownClass || "radio-button-down";
-    const buttonUpClass = this.props.buttonUpClass || "radio-button-up";
+    const buttonUpClass   = this.props.buttonUpClass   || "radio-button-up";
+    const buttonDownClass = this.props.buttonDownClass || "radio-button-down";    
 
     return (
       <div
         id={this.props.id}
         style={ (this.props.pressed ? {backgroundColor:this.props.buttonColor} : {borderColor:this.props.buttonColor}) } 
         className={this.props.pressed ? buttonDownClass : buttonUpClass}
-        onClick={() => {
-          this.props.callStateContainerRadioButtonPressed(this.props.id);
-        }}>
+        onClick={() => this.props.callStateContainerRadioButtonPressed(this.props.id)} >
         {this.props.caption}
       </div>
     );
