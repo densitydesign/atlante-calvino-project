7b1abf6ea6c8bd89c702ed765eaff2af2e88294e
crillion <crillion@tiscali.it>
2020-02-06 12:06:02 +0100

    intro : fixed a layout issue for cells without background

--- src/views/AtlasIntro/IntroViewSelector.js
+++ src/views/AtlasIntro/IntroViewSelector.js
@@ -19,7 +19,10 @@ export default class IntroViewSelector extends React.Component
       width : "100%",
       height : "100%"
     } : 
-    {};
+    {
+      width : "100%",
+      height : "100%"
+    };
 
     return (
 
