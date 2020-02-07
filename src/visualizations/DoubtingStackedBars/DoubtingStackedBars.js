e2c4b2223dc193869c894a9f45a7b0bc759b250b
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-02-07 12:56:29 -0500

    visualization

---
+++ src/visualizations/DoubtingStackedBars/DoubtingStackedBars.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import V from './stackedBars.d3'
+
+class DoubtingStackedBars extends Component {
+
+  componentDidMount() {
+    const data_for_update = {
+      data: this.props.data,
+      stackMode: this.props.stackMode
+    }
+    V.initialize(this._rootNode, data_for_update);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.data !== this.props.data || prevProps.stackMode !== this.props.stackMode) {
+      console.log("new data")
+      V.update(this.props.data, this.props.stackMode);
+    }
+  }
+
+  componentWillUnmount() {
+    // console.log('componentWillUnmount')
+    V.destroy(this._rootNode);
+  }
+
+  _setRef(componentNode) {
+    this._rootNode = componentNode;
+  }
+
+  render() {
+    const style={
+      width: '100%',
+      height: '100%'
+    }
+    return <svg id={this.props.id} style={style} ref={this._setRef.bind(this)}></svg>
+  }
+}
+
+export default DoubtingStackedBars;
---
+++ src/visualizations/DoubtingStackedBars/stackedBars.d3.js
@@ -0,0 +1,130 @@
+import * as d3 from 'd3';
+
+const V = {}
+
+let width,
+    height,
+    margin = {top: 20, right: 10, bottom: 40, left: 40},
+    stackModeProperties = {
+        "normalized": ["dubbio_perc", "misto_perc", "soggetto_perc", "definitivo_perc"],
+        "absolute": ["dubbio", "misto", "soggetto", "definitivo"]
+    },
+    
+    svg,
+    g,
+    serie,
+
+    x = d3.scaleBand().padding(0.2),
+    xAxis,
+    xAxisCall = d3.axisBottom(x),
+    y = d3.scaleLinear(),
+    yAxis,
+    yAxisCall = d3.axisLeft(y).ticks(null, "s"),
+    color = d3.scaleOrdinal()
+        .domain(stackModeProperties.absolute)
+        .range(["#9469bf","#69bfbf","#94bf69","#cccccc"])
+        .unknown("#ccc")
+
+V.initialize = (el, data_for_update) => {
+    console.log("initialize dubbio fase 2")
+    // console.log(el)
+
+
+    width = d3.select(el).node().getBoundingClientRect().width;
+    height = d3.select(el).node().getBoundingClientRect().height;
+
+    svg = d3.select(el);
+    g = svg.append("g");
+    serie = g.selectAll(".serie")
+
+    x.range([margin.left, width - margin.right]);
+    xAxis = svg.append("g").classed("axis x-axis", true)
+        .attr("transform", `translate(0,${height - margin.bottom})`);
+
+    y.range([height - margin.bottom, margin.top]);
+    yAxis = svg.append("g").classed("axis y-axis", true)
+        .attr("transform", `translate(${margin.left},0)`);
+
+    // Run update to populate viz
+    V.update(data_for_update.data, data_for_update.stackMode)
+}
+
+V.update = (data, stackMode) => {
+    console.log("update dubbio fase 2")
+
+    let series = d3.stack().keys(stackModeProperties[stackMode])(data)
+
+    x.domain(data.map(d => d.id));
+    xAxis.call(xAxisCall.tickValues(data.map(d => d.id)))
+        .call(g => xAxis.selectAll(".domain").remove())
+        .call(g => xAxis.selectAll(".tick").style("display", "none"));
+
+    y.domain([0, d3.max(series, d => d3.max(d, d => d[1]))]);
+    yAxis.call(yAxisCall)
+        .call(g => yAxis.selectAll(".domain").remove());
+
+    serie = serie.data(series)
+    serie.exit().remove()
+    serie = serie.enter().append("g")
+        .classed("serie", true)
+            .join("g")
+        .attr("fill", d => color(d.key.replace("_perc","")))
+        .selectAll("rect")
+        .data(d => d)
+        .join("rect")
+            .attr("x", d => x(d.data.id))
+            .attr("y", d => y(d[1]))
+            .attr("height", d => y(d[0]) - y(d[1]))
+            .attr("width", x.bandwidth())
+            .style("opacity", .8)
+            .on("mouseenter", d=>preSelection(d))
+            .on("mouseleave", d=>removePreSelection(d))
+            .on("click", (d)=>selection(d));
+    
+    const preSelection = (d) => {
+        const bar = d3.selectAll(".serie > rect").filter(rect=>rect.data.id===d.data.id);
+        bar.style("opacity", 1);
+        xAxis.selectAll(".tick").filter(tick=>tick===d.data.id).style("display", "block")
+    }
+
+    const removePreSelection = (d) => {
+        const bar = d3.selectAll(".serie > rect").filter(rect=>rect.data.id===d.data.id)
+        bar.style("opacity", .8)
+        xAxis.selectAll(".tick").filter(tick=>tick===d.data.id).style("display", "none")
+    }
+
+    const selection = (d) => {
+        removeSelection();
+
+        const width_factor = 20;
+        const bar = d3.selectAll(".serie > rect").filter(rect=>rect.data.id===d.data.id)
+        bar.transition()
+            .duration(0)
+            .attr("width", x.bandwidth()*width_factor)
+            .style("opacity", 1);;
+
+        const bar_index = data.map(d=>d.id).indexOf(d.data.id);
+        const direction = bar_index*2>data.length?-1:1;
+
+        const bars_to_move = d3.selectAll(".serie > rect").filter((rect)=>{
+            const this_index = data.map(d=>d.id).indexOf(rect.data.id)
+            return direction===1?this_index > bar_index:this_index <= bar_index
+        })
+        bars_to_move.transition()
+            .duration(0)
+            .attr("transform", `translate(${x.bandwidth()*(width_factor-1)*direction},0)`);
+    }
+
+    const removeSelection = () => {
+        const allBars = d3.selectAll(".serie > rect");
+        allBars.attr("transform", `translate(0,0)`)
+            .attr("width", x.bandwidth())
+            .style("opacity", .8);
+    }
+}
+
+V.destroy = () => {
+    console.log("destroy stacked bars dubbio fase 2")
+}
+
+export default V
