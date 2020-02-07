1932e00fa042d8ec0bb8ffe7ce71edb65189a292
crillion <crillion@tiscali.it>
2020-02-07 17:10:54 +0100

    splash screen : added hand down icon

--- src/visualizations/SplashScreen/SplashScreenWrapper.js
+++ src/visualizations/SplashScreen/SplashScreenWrapper.js
@@ -5,6 +5,9 @@ import SplashScreen from './SplashScreen';
 
 import { Link } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHandPointDown } from '@fortawesome/free-solid-svg-icons';
+
 import "./SplashScreen.js";
 
 export default class SplashScreenWrapper extends React.Component
@@ -18,6 +21,7 @@ export default class SplashScreenWrapper extends React.Component
         <SplashScreen />
         <h2 className="splash-screen-caption">Letteratura e visualizzazione</h2>
         <h3 className="splash-screen-description">Eine wunderbare Heiterkeit hat meine ganze Seele eingenommen, gleich den süßen</h3>
+        <FontAwesomeIcon className="splash-screen-hand-icon" icon={faHandPointDown} />
       </Link>
     );
   }
