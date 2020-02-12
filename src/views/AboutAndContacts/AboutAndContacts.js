1ca92ce3906dadb87325f3cdb99c11c465975261
Beatrice Gobbo <gobbo.beatrice92@gmail.com>
2020-02-12 16:14:23 +0100

    add pictures to background

--- src/views/AboutAndContacts/AboutAndContacts.js
+++ src/views/AboutAndContacts/AboutAndContacts.js
@@ -27,7 +27,7 @@ export default class AboutAndContacts extends React.Component
               <br/>
                 <br/>
             <p>
-            L'équipe letteraria del progetto lavora all'interno dell'Unità d'italiano del Département de langues et littératures romanes dell'Université de Genève.
+            L'équipe letteraria del progetto lavora all'interno dell'Unità d'italiano <span id="overlay"><img src={process.env.PUBLIC_URL + '/imagesPop/fiabeita.jpg'} className="floatLeft" alt="information complement" /></span> del Département de langues et littératures romanes dell'Université de Genève.
             <br/>Il gruppo riunisce specialiste di critica letteraria e in particolare dell'opera di Italo Calvino, che affronteranno l'analisi dell'intero corpus narrativo calviniano, articolando i risultati della loro ricerca in modalità compatibili con gli strumenti di visualizzazione sviluppati dal DensityDesign Research Lab.</p>
             <br/>
             <br/>
@@ -44,7 +44,8 @@ export default class AboutAndContacts extends React.Component
             Dipartimento di Design, Politecnico di Milano</strong><br/></h5>  <br/>
                 <br/>
             <p>Il laboratorio di ricerca DensityDesign è parte del Dipartimento di Design del Politecnico di Milano.
-            Obiettivo del laboratorio è la rappresentazione di fenomeni complessi di natura sociale, organizzativa o urbana. A questi temi va aggiunto il crescente interesse per le Digital Humanities, ed in particolare per tutto ciò che sta all'intersezione fra le materie umanistiche e l'information design, come la progettazione di artefatti e metodi digitali per l'elaborazione, l'accesso e la visualizzazione dell'informazione.</p>
+            Obiettivo del laboratorio è la rappresentazione <span id="overlay"><img src={process.env.PUBLIC_URL + '/imagesPop/visconte-orange.jpg'} className="sizein" alt="information complement" style={{"margin-left":"10em"}} /></span>
+di fenomeni complessi di natura sociale, organizzativa o urbana. A questi temi va aggiunto il crescente interesse per le Digital Humanities, ed in particolare per tutto ciò che sta all'intersezione fra le materie umanistiche e l'information design, come la progettazione di artefatti e metodi digitali per l'elaborazione, l'accesso e la visualizzazione dell'informazione.</p>
 
             <br/>
             <br/>
@@ -67,6 +68,7 @@ export default class AboutAndContacts extends React.Component
                 <li>Mail: <a href="mailto:atlantecalvino@unige.ch">atlantecalvino@unige.ch</a></li>
                 <li>Twitter: <a href="https://twitter.com/intent/tweet?screen_name=AtlanteCalvino&ref_src=twsrc%5Etfw" className="twitter-mention-button" data-show-count="false">@AtlanteCalvino</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></li>
               </ul>
+              <span id="overlay"><img src={process.env.PUBLIC_URL + '/imagesPop/lezioni.jpg'} className="floatTop" alt="information complement" />ciao</span>
             </address>
           </div>
         </div>
