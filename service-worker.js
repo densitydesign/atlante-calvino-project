0b57daae07ca3837e3e83953e9e59921901c802d
crillion <crillion@tiscali.it>
2020-02-13 12:13:22 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/atlante-calvino-project/precache-manifest.402c76a559eb3daec90191db52f18b6e.js"
+  "/atlante-calvino-project/precache-manifest.7fd56abef523bd37332d3652f984584f.js"
 );
 
 self.addEventListener('message', (event) => {
