a92848b5dc211d58b5e01926fa8f512646fe9877
crillion <crillion@tiscali.it>
2020-02-19 15:25:24 +0100

    hamburger menu / now the intro link points to the splash screen

--- src/general/IntroLink/IntroLink.js
+++ src/general/IntroLink/IntroLink.js
@@ -8,7 +8,7 @@ export default class IntroLink extends React.Component
 {
   render()
   {
-    const route = "/AtlasIntro";
+    const route = "/";
     return (
       <div className="intro-link" style={this.props.style} >
         <Link to={route}><h4>INTRO</h4></Link>
