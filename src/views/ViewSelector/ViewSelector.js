f994c508691f90581ce525398e3e7d18bf5f6d51
crillion <crillion@tiscali.it>
2020-02-20 11:20:45 +0100

    view selector : made cells entirely clickable (rather than only in the text)

--- src/views/ViewSelector/ViewSelector.js
+++ src/views/ViewSelector/ViewSelector.js
@@ -6,43 +6,35 @@ export default class ViewSelector extends React.Component
 {
   render()
   {
-    let children = React.Children.toArray(this.props.children)
-    let the_link = this.props.route.includes("#") ? <HashLink to={this.props.route}>{this.props.text}</HashLink> : <Link to={this.props.route}>{this.props.text}</Link>
-    let content;
-    if (this.props.subtitle) content =
-    (
-      <>
-        {the_link}
-        <h2>{this.props.subtitle}</h2>
-      </>
-    )
-    else content = the_link;
+    const children = React.Children.toArray(this.props.children);
 
-    if (children.length < 1) {
-      let obj = { type: 'span'}
-      children.push(obj)
-    }
+    if(children.length < 1) children.push({ type : "span" });
 
     const style = this.props.image ? { backgroundImage : "url('" + process.env.PUBLIC_URL + this.props.image + "')" } : {};
 
-    return (
-
-      <div className={this.props.className} style={style} >
+    const innerPart = (
+      <div>
         {
-          children.map((d,i)=>{
-            // console.log(d);
-            let this_class = ""
-            if (d.props && d.props.dataClass) {
-              this_class = d.props.dataClass
-            }
-            if (this.props.subtitle)
-            return content
+          children.map((d, i) =>
+          {
+            const this_class = d.props && d.props.dataClass ? d.props.dataClass : "";
 
-            else
-            return <d.type key={i} className={this_class}>{content}</d.type>
+            return (
+              <d.type key={i} className={this_class}>
+                {this.props.text}
+                <h2>{this.props.subtitle}</h2>
+              </d.type>
+            );
           })
         }
       </div>
     );
+
+    const result = 
+      this.props.route.includes("#") ? 
+      <HashLink to={this.props.route} className={this.props.className} style={style} >{innerPart}</HashLink> :
+          <Link to={this.props.route} className={this.props.className} style={style} >{innerPart}</Link>;
+
+    return result;
   }
 }
