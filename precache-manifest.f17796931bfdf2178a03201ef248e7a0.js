890fd7780652046b3d7b0ca9e404d09a5f65bd8f
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-05-07 13:32:01 +0200

    Updates

---
+++ precache-manifest.f17796931bfdf2178a03201ef248e7a0.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3ccff354e3707511d874f8507b9d70",
+    "url": "/atlante-calvino-project/index.html"
+  },
+  {
+    "revision": "3345cafe13e19c498fe1",
+    "url": "/atlante-calvino-project/static/css/2.fb4a9bca.chunk.css"
+  },
+  {
+    "revision": "7f64af3e1f18809c2ffd",
+    "url": "/atlante-calvino-project/static/css/main.ebedfcb6.chunk.css"
+  },
+  {
+    "revision": "3345cafe13e19c498fe1",
+    "url": "/atlante-calvino-project/static/js/2.c035ad91.chunk.js"
+  },
+  {
+    "revision": "a8f679b84aa9489fe90890f4139e2501",
+    "url": "/atlante-calvino-project/static/js/2.c035ad91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f64af3e1f18809c2ffd",
+    "url": "/atlante-calvino-project/static/js/main.14567fa0.chunk.js"
+  },
+  {
+    "revision": "87e7e2ec5ff70ca1c4b5",
+    "url": "/atlante-calvino-project/static/js/runtime-main.401f0e3f.js"
+  },
+  {
+    "revision": "1e3f889935d7adb2ec5515487622acc8",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Bold.1e3f8899.woff2"
+  },
+  {
+    "revision": "ddc9df11efe9abf1d6bd014194dd7067",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Bold.ddc9df11.woff"
+  },
+  {
+    "revision": "e9f9ad4379deff60d497e38fb3a061a2",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Bold.e9f9ad43.eot"
+  },
+  {
+    "revision": "48a88ec238a746bb67afedc96a315b6b",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Light.48a88ec2.eot"
+  },
+  {
+    "revision": "789f9dbc9f314f4567a913fc8f8ed240",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Light.789f9dbc.woff2"
+  },
+  {
+    "revision": "cf7bd856854377efcd4318086d8a176b",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Light.cf7bd856.woff"
+  },
+  {
+    "revision": "392c3a8d6c7561636c97a531cd8fa408",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Medium.392c3a8d.woff2"
+  },
+  {
+    "revision": "61ac35992aa712c9d7a0c434f2a908ff",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Medium.61ac3599.eot"
+  },
+  {
+    "revision": "d0571e7badae4b745b48a4fb91ea94b5",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Medium.d0571e7b.woff"
+  },
+  {
+    "revision": "05ab3aed45a8ecee54db0793544f2326",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Regular.05ab3aed.woff2"
+  },
+  {
+    "revision": "2fab8dc6ef36fea1ebc093eee5fb627f",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Regular.2fab8dc6.eot"
+  },
+  {
+    "revision": "adf8c12e7d0fcba5c9bd683308a35b5b",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-Regular.adf8c12e.woff"
+  },
+  {
+    "revision": "c9742542b455962269cc142415b1d41e",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-SemiBold.c9742542.eot"
+  },
+  {
+    "revision": "e2f89cec1defbba840205e8da436172f",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-SemiBold.e2f89cec.woff2"
+  },
+  {
+    "revision": "f3587a8d91aa186e77a882f08bf8127d",
+    "url": "/atlante-calvino-project/static/media/HKGrotesk-SemiBold.f3587a8d.woff"
+  },
+  {
+    "revision": "30a61c89a252a47a8743367d2a34002b",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-bolditalic.30a61c89.eot"
+  },
+  {
+    "revision": "74505b4c5efcf950ff5aee1aa457121c",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-bolditalic.74505b4c.woff"
+  },
+  {
+    "revision": "85d68e94d8d7bf70ec809a680c6c1299",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-bolditalic.85d68e94.woff2"
+  },
+  {
+    "revision": "247a0a3ae80ccf592b6bfb07264b54ad",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-italic.247a0a3a.eot"
+  },
+  {
+    "revision": "6e0c778fb760affc0a3204968b622bdd",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-italic.6e0c778f.woff"
+  },
+  {
+    "revision": "c1ebfdefd3bf106837826f4a66ed4eaa",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-italic.c1ebfdef.woff2"
+  },
+  {
+    "revision": "0069ac5c99b495897c2b02f96a68ba68",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-lightitalic.0069ac5c.eot"
+  },
+  {
+    "revision": "04fe47df3ace15019bb39f4a138ea316",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-lightitalic.04fe47df.woff"
+  },
+  {
+    "revision": "062b2fc082a5c79ff0464e178f4feefe",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-lightitalic.062b2fc0.woff2"
+  },
+  {
+    "revision": "9ab2790df1586ab66a95c911648ea6fb",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-mediumitalic.9ab2790d.eot"
+  },
+  {
+    "revision": "dc54af88b68a187632252633c1467be2",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-mediumitalic.dc54af88.woff"
+  },
+  {
+    "revision": "ddb992f3f7419760753fe28a60d56004",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-mediumitalic.ddb992f3.woff2"
+  },
+  {
+    "revision": "03a1355aed8745a5a447be65192a8d17",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-semibolditalic.03a1355a.woff"
+  },
+  {
+    "revision": "ab3956a1444a69234bb9dad853389164",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-semibolditalic.ab3956a1.eot"
+  },
+  {
+    "revision": "bef7d8c3979569f325ab882c7d563fab",
+    "url": "/atlante-calvino-project/static/media/hkgrotesk-semibolditalic.bef7d8c3.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/atlante-calvino-project/precache-manifest.848c795b48fceb37f7c2bbd7625ed440.js"
+  "/atlante-calvino-project/precache-manifest.f17796931bfdf2178a03201ef248e7a0.js"
 );
 
 self.addEventListener('message', (event) => {
