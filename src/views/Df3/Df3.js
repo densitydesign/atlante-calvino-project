55d65680905451005c20b943c9bac11ec5bfaf85
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-05-19 10:00:53 +0200

    init metaballs

--- src/views/Df3/Df3.js
+++ src/views/Df3/Df3.js
@@ -9,7 +9,8 @@ const   simulation = d3.forceSimulation([]),
             x: d3.scaleBand().range([0,1]).domain(['negazione','dubbio','riformulazione']),
             y: d3.scaleBand().range([0,1]).domain(['cosa','come','senso'])
         },
-        matrix_color = d3.scaleLinear().range(['#000','#f00']).domain([0,75]).clamp(true);
+        matrix_color = d3.scaleLinear().range(['#000','#f00']).domain([0,75]).clamp(true),
+        col_macrocategorie = d3.scaleOrdinal().range(['#FF3366', '#FFD93B', '#10BED2']).domain(['cosa','come','senso']);
 
 let     trail;
 
@@ -24,37 +25,53 @@ class Df3 extends Component {
     this._rootNode = componentNode;
   }
   async componentDidMount() {
-    const _data = await (await d3.csv(process.env.PUBLIC_URL + '/✅ Dataset Fase 3 - flagged - mds.csv')).filter(d=>{return d.id!=='V000a'&&d.id!=='V000b'});
-    // const _occurrences = await (await d3.csv(process.env.PUBLIC_URL + '/✅ Dataset Fase 3 - flagged - mds.csv'));
+    
+    let _data = await (await d3.csv(process.env.PUBLIC_URL + '/✅ Dataset Fase 3 - flagged - mds.csv'))
 
     let width = this._rootNode.getBoundingClientRect().width,
         height = this._rootNode.getBoundingClientRect().height,
         margin= {},
-        length = d3.scaleLinear().range([0,30]).domain([0,d3.max(_data,d=>+d.length)]),
-        lengthClumped = (l)=>d3.max([5,length(Number(l))]),
-        percentage = d3.scaleLinear().range([0.05,1]).domain([0.1,51]),
+        radius = d3.scalePow().exponent(0.5).range([0,25]).domain([0,d3.max(_data,d=>+d.length)]),
+        radiusClumped = (l)=>d3.max([,radius(Number(l))]),
+        // percentage = d3.scaleLinear().range([0.05,1]).domain([0.1,51]),
         master_g, g, node;
 
-    // matrix_color.domain([
-    //     0,
-    //     d3.max([
-    //         d3.max(_data,d=>Number(d['cont_occ_cosa-dubbio'])),
-    //         d3.max(_data,d=>Number(d['cont_occ_come-negazione'])),
-    //         d3.max(_data,d=>Number(d['cont_occ_come-riformulazione'])),
-    //         d3.max(_data,d=>Number(d['cont_occ_cosa-dubbio'])),
-    //         d3.max(_data,d=>Number(d['cont_occ_cosa-negazione'])),
-    //         d3.max(_data,d=>Number(d['cont_occ_cosa-riformulazione'])),
-    //         d3.max(_data,d=>Number(d['cont_occ_senso-dubbio'])),
-    //         d3.max(_data,d=>Number(d['cont_occ_senso-negazione'])),
-    //         d3.max(_data,d=>Number(d['cont_occ_senso-riformulazione']))
-    //     ])
-    // ]);
+    // radiusClumped = radius;
+
+    _data = _data.filter(d=>{return d.id!=='V000a'&&d.id!=='V000b'}).map(d=>{
+        d.color_macrocategorie=color_macrocategorie(d);
+        d.color_manifestazioni_stilistiche=color_manifestazioni_stilistiche(d);
+
+        d.combinations  =   [   "come-dubbio", "come-negazione", "come-riformulazione",
+                                "cosa-dubbio", "cosa-negazione", "cosa-riformulazione",
+                                "senso-dubbio", "senso-negazione", "senso-riformulazione"
+                            ].map(p=>{
+
+                                let obj={};
+                                obj['id']=d['ID opera'];
+                                obj.type=p;
+
+                                obj.size=radiusClumped(Number(d['cont_car_'+p]));
+
+                                obj.mx  = matrix_grid.x(p.split('-')[1]) * radiusClumped(d.length)
+                                        + radiusClumped(d.length)/6
+                                        - obj.size/2;
+
+                                obj.my  = matrix_grid.y(p.split('-')[0]) * radiusClumped(d.length) 
+                                        + radiusClumped(d.length)/6
+                                        - obj.size/2;
+
+                                return obj;
+                            });
+
+        return d;
+    });
 
     simulation
-        .force('x',d3.forceX(d=>p(+d.prop_occ_x)))
-        .force('y',d3.forceY(d=>p(-d.prop_occ_y)))
-        // .force('collision',d3.forceCollide( d=>d3.max([2,length(+d.length)])+1 ))
-        .force('collision-rect', rectCollide().size(function (d) { return [lengthClumped(d.length)+4, lengthClumped(d.length)+4] }))
+        .force('x',d3.forceX(d=>p(+d.prop_occ_perc_x)))
+        .force('y',d3.forceY(d=>p(-d.prop_occ_perc_y)))
+        .force('collision',d3.forceCollide( d=>radiusClumped(d.length) ))
+        // .force('collision-rect', rectCollide().size(function (d) { return [lengthClumped(d.length)+4, lengthClumped(d.length)+4] }))
         .alphaMin(0.1)
         .on('tick',()=>{
             node.attr('transform',d=>`translate(${d.x}, ${d.y})`)
@@ -72,28 +89,40 @@ class Df3 extends Component {
                         .merge(trail)
                         .attr('d',d=>d3.line()(d.trail));
         })
-        .on('end',()=>{
-            console.log(_data)
-            console.log(JSON.stringify(_data.map(d=>{return{'id':d['ID opera'],'x':d.x,'y':d.y,'r':lengthClumped(d.length)}})),null,2)
-        })
+        // .on('end',()=>{
+        //     console.log(_data)
+        //     console.log(JSON.stringify(_data.map(d=>{return{'id':d['ID opera'],'x':d.x,'y':d.y,'r':lengthClumped(d.length)}})),null,2)
+        // })
         .stop();
-
+    
     const svg = d3.select(this._rootNode).select('svg');
 
-
     svg.attr('width',width).attr('height',height);
+    const defs = svg.append('defs')
+        .html(`
+            <filter id="blur">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="2" result="blur" />
+                <feComposite in="SourceGraphic" in2="matrix" operator="atop"/>
+            </filter>
+
+            <filter id="gooey-effect-cluster">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="1" color-interpolation-filters="sRGB" result="blur"/>
+                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 19 -9" result="gooey"/>
+            </filter>
+        `);
     master_g = svg.append('g');
     g = master_g.append('g').attr('transform',`translate(${width/2},${height/2})`);
     trail = g.append('g').classed('trails-group',true).selectAll('.trail');
+    
     node = g.selectAll('.node').data(_data)
         .enter().append('g')
             .classed('node',true)
             .attr('data-id',d=>d.id)
-            // .call(d3.drag()
-            //     .on("start", dragstarted)
-            //     .on("drag", dragged)
-            //     .on("end", dragended)
-            // )
+            .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended)
+            )
             .on('mouseenter',function(){
                 const selectedNodes = node.filter(function(){return d3.select(this).classed('selected')});
                 if (selectedNodes.size()>0){
@@ -141,41 +170,72 @@ class Df3 extends Component {
         d.fy = null;
     }
 
-    node.append('rect')
-        .attr('fill','#000')
-        // .attr('stroke', '#aaa')
-        .attr('opacity',d=>percentage(d.percentuale_dubbio))
-        .attr('width',d=>lengthClumped(d.length))
-        .attr('height',d=>lengthClumped(d.length))
+    node.append('circle')
+        .attr('r',d=>radiusClumped(d.length))
+        .attr('fill','white')
+        .attr('stroke-width',0.5)
+        .attr('stroke-dasharray','1 2')
+        .attr('stroke','#666');
 
-    node.append('g').selectAll('rect').data(d=>{
-      let arr = [
-        "come-dubbio", "come-negazione", "come-riformulazione",
-        "cosa-dubbio", "cosa-negazione", "cosa-riformulazione",
-        "senso-dubbio", "senso-negazione", "senso-riformulazione"
-      ].map(p=>{
-          let obj={};
-          obj['id']=d['ID opera'];
-          obj.type=p;
-          obj.valueColor=Number(d['cont_occ_'+p]);
-          obj.size=Number(d['prop_occ_'+p])*lengthClumped(d.length)/3;
+    // node.append('rect')
+    //     .attr('fill','#000')
+    //     // .attr('stroke', '#aaa')
+    //     .attr('opacity',d=>percentage(d.percentuale_dubbio))
+    //     .attr('width',d=>lengthClumped(d.length))
+    //     .attr('height',d=>lengthClumped(d.length))
 
-          obj.x =   matrix_grid.x(p.split('-')[1]) * lengthClumped(d.length)
-                    + lengthClumped(d.length)/6
-                    - obj.size/2;
-          obj.y =   matrix_grid.y(p.split('-')[0]) * lengthClumped(d.length) 
-                    + lengthClumped(d.length)/6
-                    - obj.size/2;
+    // node.append('g').selectAll('rect').data(d=>{
+    //   let arr = [
+    //     "come-dubbio", "come-negazione", "come-riformulazione",
+    //     "cosa-dubbio", "cosa-negazione", "cosa-riformulazione",
+    //     "senso-dubbio", "senso-negazione", "senso-riformulazione"
+    //   ].map(p=>{
+    //       let obj={};
+    //       obj['id']=d['ID opera'];
+    //       obj.type=p;
+    //       obj.valueColor=Number(d['cont_occ_'+p]);
+    //       obj.size=Number(d['prop_occ_'+p])*lengthClumped(d.length)/3;
 
-          return obj;
-        });
-      return arr;
-    }).enter().append('rect')
-      .attr('width',d=>d.size)
-      .attr('height',d=>d.size)
-      .attr('x',d=>d.x)
-      .attr('y',d=>d.y)
-      .attr('fill',d=>matrix_color(d.valueColor));
+    //       obj.x =   matrix_grid.x(p.split('-')[1]) * lengthClumped(d.length)
+    //                 + lengthClumped(d.length)/6
+    //                 - obj.size/2;
+    //       obj.y =   matrix_grid.y(p.split('-')[0]) * lengthClumped(d.length) 
+    //                 + lengthClumped(d.length)/6
+    //                 - obj.size/2;
+
+    //       return obj;
+    //     });
+    //   return arr;
+    // }).enter().append('rect')
+    //   .attr('width',d=>d.size)
+    //   .attr('height',d=>d.size)
+    //   .attr('x',d=>d.x)
+    //   .attr('y',d=>d.y)
+    //   .attr('fill',d=>matrix_color(d.valueColor));
+
+    node.each(function(d){
+        //console.log(d)
+        const combination = d3.select(this).append('g')
+            .attr('filter','url(#gooey-effect-cluster)')
+            .selectAll('.combination')
+            .data(d=>d.combinations)
+            .enter()
+            .append('circle')
+            .attr('r',d=>d.size)
+            .attr('fill',d=>color_macrocategorie(d));
+        
+        const this_simulation = d3.forceSimulation(combination.data())
+            .force('x',d3.forceX(0))
+            .force('y',d3.forceY(0))
+            .force('collision', d3.forceCollide(d=>d.size))
+            .on('tick', ()=>{
+                combination.attr('cx',d=>d.x).attr('cy',d=>d.y);
+            })
+            .alpha(1)
+            .alphaMin(0.1)
+            .restart();
+
+    }) // node each
       
     const font_size = 8;
     node.append('text')
@@ -191,7 +251,7 @@ class Df3 extends Component {
         .attr('font-size',font_size*1.5)
         // .attr('x',d=>0.5*lengthClumped(d.length))
         // .attr('y', d=>-lengthClumped(d.length)/2 -1)
-        .text(d=>d.title)
+        .text(d=>d.title + '-' +d.year)
 
     simulation.nodes(_data).alpha(1).restart();
 
@@ -253,7 +313,7 @@ class Df3 extends Component {
     return <div style={{width:'100vw',height:'100vh'}} ref={this._setRef.bind(this)}>
         <svg />
         <p style={{position:'absolute',top:0,left:0, fontSize:12}}>
-            <select id="mds-positioning" onChange={(event)=>this.setState({'positioning':event.target.value})}>
+            <select defaultValue={'prop_occ_perc'} id="mds-positioning" onChange={(event)=>this.setState({'positioning':event.target.value})}>
                 <option value="prop_occ">MDS 003 - Proporzione fra tipi di occorrenze</option>
                 <option value="cont_occ">MDS 003 - Conteggio tipi di occorrenze</option>
                 <option value="cont_e_prop_occ">MDS 003 - Proporzione e conteggio tipi di occorrenze</option>
@@ -373,4 +433,60 @@ function rectCollide() {
 
 function constant(_) {
   return function () { return _ }
+}
+
+function color_macrocategorie(d) {
+    let col_r = 0;
+    let col_g = 0;
+    let col_b = 0;
+
+    const tot_cosa = 1*(Number(d['prop_occ_cosa-dubbio']) + Number(d['prop_occ_cosa-negazione']) + Number(d['prop_occ_cosa-riformulazione']));
+    const tot_come = 1*(Number(d['prop_occ_come-dubbio']) + Number(d['prop_occ_come-negazione']) + Number(d['prop_occ_come-riformulazione']));
+    const tot_senso = 1*(Number(d['prop_occ_senso-dubbio']) + Number(d['prop_occ_senso-negazione']) + Number(d['prop_occ_senso-riformulazione']));
+
+    col_r   =   tot_cosa * d3.color(col_macrocategorie('cosa')).r
+            +   tot_come * d3.color(col_macrocategorie('come')).r
+            +   tot_senso * d3.color(col_macrocategorie('senso')).r;
+
+    col_g   =   tot_cosa * d3.color(col_macrocategorie('cosa')).g
+            +   tot_come * d3.color(col_macrocategorie('come')).g
+            +   tot_senso * d3.color(col_macrocategorie('senso')).g;
+
+    col_b   =   tot_cosa * d3.color(col_macrocategorie('cosa')).b
+            +   tot_come * d3.color(col_macrocategorie('come')).b
+            +   tot_senso * d3.color(col_macrocategorie('senso')).b;
+
+    // d.color_macrocategorie = d3.rgb(col_r, col_g, col_b);
+
+    return color_macrocategorie;
+}
+
+function color_manifestazioni_stilistiche(d) {
+    let col_r = 0;
+    let col_g = 0;
+    let col_b = 0;
+
+    // dubbio
+    // negazione
+    // riformulazione
+
+    const tot_dubbio = 1*(Number(d['prop_occ_cosa-dubbio']) + Number(d['prop_occ_come-dubbio']) + Number(d['prop_occ_senso-dubbio']));
+    const tot_negazione = 1*(Number(d['prop_occ_cosa-negazione']) + Number(d['prop_occ_come-negazione']) + Number(d['prop_occ_senso-negazione']));
+    const tot_riformulazione = 1*(Number(d['prop_occ_cosa-riformulazione']) + Number(d['prop_occ_come-riformulazione']) + Number(d['prop_occ_senso-riformulazione']));
+
+    col_r   =   tot_dubbio * d3.color('#8AE297').r
+            +   tot_negazione * d3.color('#FFA500').r
+            +   tot_riformulazione * d3.color('#BBBBFF').r;
+    
+    col_g   =   tot_dubbio * d3.color('#8AE297').g
+            +   tot_negazione * d3.color('#FFA500').g
+            +   tot_riformulazione * d3.color('#BBBBFF').g;
+
+    col_b   =   tot_dubbio * d3.color('#8AE297').b
+            +   tot_negazione * d3.color('#FFA500').b
+            +   tot_riformulazione * d3.color('#BBBBFF').b;
+
+    d.color_manifestazioni_stilistiche = d3.rgb(col_r, col_g, col_b);
+
+    return d.color_manifestazioni_stilistiche   
 }
