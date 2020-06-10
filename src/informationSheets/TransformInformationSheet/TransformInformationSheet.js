02eb7e7c1ebf0a881dde067fcda04eb4ac58f240
angelesbri1s <angelesbriones@gmail.com>
2020-06-10 15:30:50 +0200

    Pages for Schede tappa 2

---
+++ src/informationSheets/TransformInformationSheet/TransformInformationSheet.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class TransformInformationSheet extends Component {
+  render() {
+    return <div>TransformInformationSheet</div>;
+  }
+}
+
+export default TransformInformationSheet;
