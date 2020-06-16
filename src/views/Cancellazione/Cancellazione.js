9edcf7148263e1a200d32708060dee701095bd62
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-06-16 13:27:56 +0200

    started interface

--- src/views/Cancellazione/Cancellazione.js
+++ src/views/Cancellazione/Cancellazione.js
@@ -6,6 +6,12 @@ import Loading from '../../general/Loading';
 import RustyViz from '../../visualizations/RustyViz';
 import RustyVizSpatialization from '../../visualizations/RustyVizSpatialization';
 
+import MainMenu from '../../general/MainMenu';
+import PageTitle from '../../general/PageTitle';
+import MoreInfo from '../../general/MoreInfo';
+import CompassButton from '../../general/CompassButton/CompassButton';
+import HelpSidePanel from '../../panels/HelpSidePanel/HelpSidePanel';
+
 class Cancellazione extends Component {
   constructor(props){
     super(props);
@@ -23,7 +29,10 @@ class Cancellazione extends Component {
   render() {
     return <>
       <div className={styles.main}>
-        <div className="top-nav navigations"></div>
+        <div className="top-nav navigations">
+          <MainMenu className = "main-menu" style = {{gridColumn: 'span 1'}}/>
+					<PageTitle title = {"Tappa 3 – Cancellare"} style = {{gridColumn: 'span 7'}}/>
+        </div>
         <div className={styles.bodyViz + ' the-body-viz'}>
           {	this.state.loading && <Loading style = {{width: '100%'}}/>}
           {	!this.state.loading && <RustyViz data={this.state.data} /> }
