5fc8a60f2eeba3a65e6f6e877b535893e40560b3
gffuma <fumagalli.gf@gmail.com>
2020-06-17 15:54:04 +0200

    Legend for circle worms

--- src/visualizations/Realismo/CircleWorms.js
+++ src/visualizations/Realismo/CircleWorms.js
@@ -1,10 +1,19 @@
 import React, { useCallback } from 'react'
 import { colorScale } from './utils'
 import { scaleLinear } from 'd3-scale'
+import { transform } from 'lodash'
+
+const LEGEND_DEG = 10
+const LABEL_SIZE_PERCENT = 0.2
+const WORM_SIZE_PERCENT = 0.4
+const LABEL_PADDING = 10
+const INNER_CIRCLE_PADDING = 10
+const INNER_CIRCLE_STROKE_WIDTH = 5
+const LEGEND_TEXT_MARGIN = 3
 
 const Worm = React.memo(
   ({ racconto, circles, radius, labelSize, wormSize, onClick, isSelected }) => {
-    const wormStart = radius - labelSize - wormSize
+    const wormStart = radius - labelSize - wormSize - LABEL_PADDING
     const wormEnd = radius - labelSize
 
     const circleRadius = wormSize / circles.length / 2
@@ -47,7 +56,7 @@ const Worm = React.memo(
             )
           })}
         </g>
-        <text x={wormEnd}>&nbsp;&nbsp;{racconto.title}</text>
+        <text x={wormEnd}>{racconto.title}</text>
       </g>
     )
   }
@@ -61,31 +70,151 @@ export default function CircleWorms({
   racconti,
   toggleSelect,
 }) {
-  const deltaAngle = 360 / racconti.length
+  const deltaAngle = (360 - LEGEND_DEG) / racconti.length
   const size = Math.min(width, height)
 
-  const handleClick = useCallback((racconto) => {
-    toggleSelect(racconto.title)
-  }, [toggleSelect])
+  const handleClick = useCallback(
+    (racconto) => {
+      toggleSelect(racconto.title)
+    },
+    [toggleSelect]
+  )
 
   const allSelected = Object.keys(selected).length === 0
 
+  const labelSize = (size / 2) * LABEL_SIZE_PERCENT
+  const wormSize = (size / 2) * WORM_SIZE_PERCENT
+  const innerRadius =
+    size / 2 - labelSize - wormSize - LABEL_PADDING - INNER_CIRCLE_PADDING
+
+  const endLineStart =
+    labelSize +
+    LABEL_PADDING +
+    wormSize +
+    INNER_CIRCLE_PADDING -
+    INNER_CIRCLE_STROKE_WIDTH / 2
+  const endLineEnd = endLineStart + 90
+
+  // 0 - 90
   return (
     <svg width={width} height={size}>
+      <line
+        y1={0}
+        y2={labelSize}
+        x1={width / 2}
+        x2={width / 2}
+        stroke="black"
+        strokeWidth={2}
+      />
+      <text
+        x={width / 2}
+        y={labelSize - LEGEND_TEXT_MARGIN}
+        className="realismo-legend-text"
+        style={{
+          transformOrigin: `${width / 2}px ${labelSize}px`,
+          transform: 'rotate(270deg)',
+        }}
+      >
+        TITOLO
+      </text>
+      <line
+        y1={labelSize + LABEL_PADDING}
+        y2={labelSize + LABEL_PADDING + wormSize}
+        x1={width / 2}
+        x2={width / 2}
+        stroke="black"
+        strokeWidth={2}
+      />
+      <text
+        x={width / 2}
+        y={labelSize + LABEL_PADDING - LEGEND_TEXT_MARGIN}
+        className="realismo-legend-text"
+        textAnchor={'end'}
+        style={{
+          transformOrigin: `${width / 2}px ${labelSize + LABEL_PADDING}px`,
+          transform: 'rotate(270deg)',
+        }}
+      >
+        FINE
+      </text>
+      <text
+        x={width / 2}
+        y={labelSize + LABEL_PADDING + wormSize - LEGEND_TEXT_MARGIN}
+        className="realismo-legend-text"
+        textAnchor={'start'}
+        style={{
+          transformOrigin: `${width / 2}px ${
+            labelSize + LABEL_PADDING + wormSize
+          }px`,
+          transform: 'rotate(270deg)',
+        }}
+      >
+        INIZIO DEL TESTO
+      </text>
+      <circle
+        cx={width / 2}
+        cy={size / 2}
+        fill={'transparent'}
+        stroke={'#ddd'}
+        strokeWidth={INNER_CIRCLE_STROKE_WIDTH}
+        r={innerRadius}
+      />
+      <line
+        y1={endLineStart}
+        y2={endLineEnd}
+        x1={width / 2}
+        x2={width / 2}
+        stroke="black"
+        strokeWidth={2}
+      />
+      <g
+        className="babu"
+        style={{
+          transform: `translate(${width / 2 - 14}px, ${endLineEnd}px) rotate(270deg)`,
+        }}
+      >
+        <text
+          x={0}
+          y={0}
+          className="realismo-legend-text"
+          textAnchor={'start'}
+        >
+          ANNO PRIMA
+        </text>
+        <text
+          x={0}
+          y={10}
+          className="realismo-legend-text"
+          textAnchor={'start'}
+        >
+          PUBBLICAZIONE
+        </text>
+      </g>
+
       <g transform={`translate(${width / 2} , ${size / 2})`}>
-        {racconti.map((racconto, i) => (
-          <g key={i} style={{ transform: `rotate(${i * deltaAngle}deg)` }}>
-            <Worm
-              isSelected={allSelected || !!selected[racconto.title]}
-              onClick={handleClick}
-              radius={size / 2}
-              labelSize={size / 10}
-              wormSize={(size / 10) * 2}
-              racconto={racconto}
-              circles={circlesMap[racconto.title]}
-            ></Worm>
-          </g>
-        ))}
+        {racconti.map((racconto, i) => {
+          let angle = i * deltaAngle + deltaAngle / 2 + 270 + LEGEND_DEG / 2
+          if (angle > 360) {
+            angle -= 360
+          }
+          return (
+            <g
+              key={i}
+              className={`worm-${i}`}
+              style={{ transform: `rotate(${angle}deg)` }}
+            >
+              <Worm
+                isSelected={allSelected || !!selected[racconto.title]}
+                onClick={handleClick}
+                radius={size / 2}
+                labelSize={(size / 2) * LABEL_SIZE_PERCENT}
+                wormSize={(size / 2) * WORM_SIZE_PERCENT}
+                racconto={racconto}
+                circles={circlesMap[racconto.title]}
+              ></Worm>
+            </g>
+          )
+        })}
       </g>
     </svg>
   )
