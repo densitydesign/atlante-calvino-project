8d89512db9adb6766b06d0d00aeeb7c90c4dbd46
Mauro Bianchi <bianchimro@gmail.com>
2020-07-14 18:49:58 +0200

    working on trama dataset normalization

--- src/visualizations/Trama2/utils.js
+++ src/visualizations/Trama2/utils.js
@@ -1,130 +1,111 @@
 //visual helpers
-import { scaleLinear } from 'd3-scale'
-import { extent } from 'd3-array'
+import { scaleLinear } from "d3-scale";
+import { extent } from "d3-array";
 
 //data management
-import groupBy from 'lodash/groupBy'
-import keyBy from 'lodash/keyBy'
-import sortBy from 'lodash/sortBy'
-import uniqBy from 'lodash/uniqBy'
-import minBy from 'lodash/minBy'
-import maxBy from 'lodash/maxBy'
-import mapValues from 'lodash/mapValues'
-import get from 'lodash/get'
-import take from 'lodash/take'
-import sum from 'lodash/sum'
-import flatten from 'lodash/flatten'
+import groupBy from "lodash/groupBy";
+import keyBy from "lodash/keyBy";
+import sortBy from "lodash/sortBy";
+import uniqBy from "lodash/uniqBy";
+import minBy from "lodash/minBy";
+import maxBy from "lodash/maxBy";
+import mapValues from "lodash/mapValues";
+import get from "lodash/get";
+import take from "lodash/take";
+import sum from "lodash/sum";
+import flatten from "lodash/flatten";
 
-import datasetLines from './dati/dataset_lines.json'
-import mappaMotivoTipologia from './dati/mappa_motivo_tipologia.json'
-import coloriPosizioni from './dati/colori_posizioni.json'
-import ordineColore from './dati/ordine_colore_y.json'
-
+import datasetLines from "./dati/dataset_lines.json";
+import mappaMotivoTipologia from "./dati/mappa_motivo_tipologia.json";
+import coloriPosizioni from "./dati/colori_posizioni.json";
+import ordineColore from "./dati/ordine_colore_y.json";
 
 export const MOTIVO_LINE_HEIGHT = 50;
 
-
-
-export const motivoExtent = extent(ordineColore, (item) => +item['ordine tipologia'])
+export const motivoExtent = extent(
+  ordineColore,
+  (item) => +item["ordine tipologia"]
+);
 
 export function makeScalaMotivoY(lineHeight) {
-
-  return scaleLinear().domain(motivoExtent).range([lineHeight, 0])
+  return scaleLinear().domain(motivoExtent).range([lineHeight, 0]);
 }
 
 export function makeVizData(scaleY) {
-  const ordineByCluster = keyBy(coloriPosizioni, 'valore')
+  const ordineByCluster = keyBy(coloriPosizioni, "valore");
   const tipologie = ordineColore.map((item) => ({
     ...item,
-    colore: get(ordineByCluster, item['cluster tipologia']),
-  }))
+    colore: get(ordineByCluster, item["cluster tipologia"]),
+  }));
   const colors = sortBy(tipologie, (item) => item.colore.ordine).map(
     (x) => x.colore.colori
-  )
-  const tipologieByTipologia = keyBy(tipologie, (item) => item.tipologia)
+  );
+  const tipologieByTipologia = keyBy(tipologie, (item) => item.tipologia);
 
   const clusterByMotivo = mapValues(
-    keyBy(mappaMotivoTipologia, 'motivo_type'),
-    (item) => item['cluster tipologie']
-  )
+    keyBy(mappaMotivoTipologia, "motivo_type"),
+    (item) => item["cluster tipologie"]
+  );
   const ordineMotivoByMotivo = mapValues(
-    keyBy(ordineColore, 'tipologia'),
-    (item) => +item['ordine tipologia']
-  )
+    keyBy(ordineColore, "tipologia"),
+    (item) => +item["ordine tipologia"]
+  );
 
+  const fixHoles = (data) => {
+    const holes = data
+      .map((item, i) => {
+        const motivo = item["motivo_type"];
+        const ordineMotivo = !!get(ordineMotivoByMotivo, motivo);
+        if (ordineMotivo) {
+          return null;
+        } else {
+          return {
+            index: i,
+            len: +item.end_motivo - item.start_motivo + 1,
+          };
+        }
+      })
+      .filter((x) => !!x);
 
-// // we must filter items with no scale associated and renormalize data
-// const datasetTmp = datasetLines.filter(item => {
-//   const motivo = item['motivo_type']
-//   return !!get(ordineMotivoByMotivo, motivo)
-// }).map(item => {
-//   const numCaratteri = +item['end_motivo'] -item['start_motivo'] + 1
-//   return {...item, numCaratteri}
-// })
+    const totHolesSize = holes.reduce((acc, hole) => acc + hole.len, 0);
 
-// function normalizeCharters(data) {
-//   let shift = []
-//   let lenHoles = data.reduce((acc, item, i) => {
-//     const motivo = item['motivo_type']
-//     const ordineMotivo = !!get(ordineMotivoByMotivo, motivo)
-//     if (ordineMotivo) {
-//       shift.push({
-//         ...item,
-//         start_motivo: item.start_motivo - acc,
-//         end_motivo: item.end_motivo - acc,
-//       })
-//       return acc
-//     } else {
-//       acc += item.end_motivo - item.start_motivo + 1
-//       return acc
-//     }
-//   }, 0)
-//   // console.log('BEFORE', lenHoles, shift)
-//   lenHoles += ((+shift[0].end_motivo) - shift[0].start_motivo) / 2
-//   lenHoles += ((+shift[shift.length - 1].end_motivo) - shift[shift.length - 1].start_motivo) / 2
-//   console.log('After', lenHoles)
+    let out = [];
+    data.forEach((datum, i) => {
+      const motivo = datum["motivo_type"];
+      const ordineMotivo = get(ordineMotivoByMotivo, motivo);
+      if (ordineMotivo !== undefined) {
+        const holesForDatum = holes.filter((item) => item.index < i);
+        const holesSize = holesForDatum.reduce(
+          (acc, hole) => acc + hole.len,
+          0
+        );
+        out.push({
+          ...datum,
+          start_motivo: +datum.start_motivo - holesSize,
+          end_motivo: +datum.end_motivo - holesSize,
+          "tot caratteri": +datum["tot caratteri"] - totHolesSize,
+        });
+      }
+    });
+    return out;
+  };
 
+  let datasetTmpByRacconto = groupBy(datasetLines, "titolo racconto");
+  datasetTmpByRacconto = mapValues(datasetTmpByRacconto, fixHoles);
+  const datasetLinesFinal = flatten(Object.values(datasetTmpByRacconto));
 
-//   return shift.map(item => ({
-//     ...item,
-//     'tot caratteri': (+item['tot caratteri']) - lenHoles
-//   }))
-// }
+  const datasetLinesNormalized = datasetLinesFinal.map((item) => {
+    const tot = +item["tot caratteri"];
+    const motivo = item["motivo_type"];
+    const cluster = get(clusterByMotivo, motivo);
+    const coloreCluster = get(ordineByCluster, `[${cluster}].colori`);
+    const ordineCluster = get(ordineByCluster, `[${cluster}].ordine`);
+    const ordineMotivo = get(ordineMotivoByMotivo, motivo);
 
-// let datasetTmpByRacconto = groupBy(datasetLines, 'titolo racconto')
-// datasetTmpByRacconto = mapValues(datasetTmpByRacconto, normalizeCharters)
-// const finalDataSet = flatten(Object.values(datasetTmpByRacconto))
-
-// console.log(finalDataSet)
-// const titoli = Object.keys(datasetTmpByRacconto)
-// datasetTmpByRacconto = titoli.reduce((acc, item) => {
-
-//   const data = datasetTmpByRacconto[item]
-//   const len = sum(data.map(item => item.numCaratteri))
-//   acc[item] = len
-//   return acc
-
-// }, {})
-
-
-// const finalDataset = datasetTmp.map(item => ({...item, len:lunghezze[item['titolo racconto']]  }))
-
-
-
-
-  const datasetLinesNormalized = datasetLines.map((item) => {
-    const tot = +item['tot caratteri']
-    const motivo = item['motivo_type']
-    const cluster = get(clusterByMotivo, motivo)
-    const coloreCluster = get(ordineByCluster, `[${cluster}].colori`)
-    const ordineCluster = get(ordineByCluster, `[${cluster}].ordine`)
-    const ordineMotivo = get(ordineMotivoByMotivo, motivo)
-
-    const startMotivoNorm = +item.start_motivo / tot
-    const endMotivoNorm = +item.end_motivo / tot
-    const y =
-      ordineMotivo !== undefined ? scaleY(ordineMotivo) : undefined
-    const x = (startMotivoNorm + endMotivoNorm) / 2
+    const startMotivoNorm = +item.start_motivo / tot;
+    const endMotivoNorm = +item.end_motivo / tot;
+    const y = ordineMotivo !== undefined ? scaleY(ordineMotivo) : undefined;
+    const x = (startMotivoNorm + endMotivoNorm) / 2;
 
     return {
       ...item,
@@ -136,24 +117,24 @@ export function makeVizData(scaleY) {
       ordineMotivo,
       y,
       x,
-    }
-  })
+    };
+  });
 
-  const byRacconto = groupBy(datasetLinesNormalized, 'titolo racconto')
-
+  const byRacconto = groupBy(datasetLinesNormalized, "titolo racconto");
+  
   let racconti = sortBy(
-    uniqBy(datasetLines, (item) => item['titolo racconto']).map((item) => ({
-      titolo: item['titolo racconto'],
-      anno: item['anno'],
-      mese: item['mese'],
+    uniqBy(datasetLines, (item) => item["titolo racconto"]).map((item) => ({
+      titolo: item["titolo racconto"],
+      anno: item["anno"],
+      mese: item["mese"],
     })),
     (item) => {
-      const anno = +item.anno
-      const mese = +item.mese
+      const anno = +item.anno;
+      const mese = +item.mese;
 
-      return `${anno.toFixed(4)}${mese.toFixed(2)}`
+      return `${anno.toFixed(4)}${mese.toFixed(2)}`;
     }
-  )
+  );
   // //#TODO: remove this (limiting for debug)
   // racconti = take(racconti, 5)
   //pre-computing filters
@@ -161,14 +142,27 @@ export function makeVizData(scaleY) {
     const minDatum = minBy(
       byRacconto[racconto.titolo],
       (item) => item.ordineMotivo
-    )
+    );
     const maxDatum = maxBy(
       byRacconto[racconto.titolo],
       (item) => item.ordineMotivo
-    )
+    );
+    const minX = byRacconto[racconto.titolo][0].x
+    const maxX = byRacconto[racconto.titolo][byRacconto[racconto.titolo].length - 1].x
+    const fixScale = scaleLinear().domain([minX, maxX]).range([0, 1])
     // console.log(123, racconto.titolo, minDatum, maxDatum)
-    return { ...racconto, minDatum, maxDatum }
-  })
+    return { ...racconto, minDatum, maxDatum, minX, maxX, fixScale };
+  });
+  
+  
+  // const raccontiByRacconto = keyBy(racconti, 'titolo')
+  // const byRaccontoRemapped = mapValues(byRacconto, (values, k) => values.map(v => ({
+  //   ...v,
+  //   x: raccontiByRacconto[k].fixScale(v.x)
+    
+  // })))
+
+  // console.log("V", byRacconto, byRaccontoRemapped)
 
   return {
     tipologie,
@@ -176,26 +170,27 @@ export function makeVizData(scaleY) {
     colors,
     racconti,
     byRacconto,
-  }
+    // byRacconto: byRaccontoRemapped,
+  };
 }
 
 export const splitPath = (d) => {
-  const pieces = d.split('C')
+  const pieces = d.split("C");
   const paths = pieces.reduce((acc, item, i) => {
     if (i === 0) {
-      return acc
+      return acc;
     }
     if (i === 1) {
-      const path = pieces[0] + 'C' + pieces[i]
-      acc.push(path)
+      const path = pieces[0] + "C" + pieces[i];
+      acc.push(path);
     } else {
-      const [a, b] = pieces[i - 1].split(',').reverse()
-      const path = `M${b},${a}C${pieces[i]}`
-      acc.push(path)
+      const [a, b] = pieces[i - 1].split(",").reverse();
+      const path = `M${b},${a}C${pieces[i]}`;
+      acc.push(path);
     }
 
-    return acc
-  }, [])
+    return acc;
+  }, []);
 
-  return paths
-}
+  return paths;
+};
