284d4da3a3d25a12fb2299863a0ccf003dd93bf7
Beatrice Gobbo <gobbo.beatrice92@gmail.com>
2020-09-21 17:18:13 +0200

    Luoghi Infosheet End

--- src/informationSheets/SpaceInformationSheet/SpaceInformationSheet.js
+++ src/informationSheets/SpaceInformationSheet/SpaceInformationSheet.js
@@ -58,7 +58,7 @@ export default class SpaceInformationSheet extends React.Component
                     Repubblica di Genova: è dunque all’interno di uno spazio reale e definito che hanno luogo le avventure fantastiche del protagonista.
                 </p>
                 <div className={SheetStyles.sideContent}>
-                    <img className={SheetStyles.image} style={{position:'sticky',top:'calc(var(--navigation-height)+2px)'}} src={zoom_03}/>
+                    <img className={SheetStyles.image} style={{position:'sticky',top:'calc(var(--navigation-height)+4em)'}} src={zoom_03}/>
                 </div>
                     <p className={SheetStyles.paragraph}>
                       In apparenza questo non accade nel <em>Barone rampante</em>, ambientato nel libero comune di Ombrosa, territorio - mondo del baronetto Cosimo Piovasco di Rondò.
@@ -66,43 +66,43 @@ export default class SpaceInformationSheet extends React.Component
                       di cui è comune tributario.Pertanto, seppur in modo meno evidente.anche nel caso del Barone rampante lo spazio fantastico si intreccia a quello storico.
                       Il fatto poi che Terralba e Ombrosa siano in qualche modo riconducibili alla Liguria, terra natale di Calvino, è doppiamente significativo.
                       Nel 1965, è lo stesso scrittore, con lo pseudonimo - anagramma di Tonio Cavilla, a sottolineare l’ importanza del paesaggio ligure sullo sfondo:
-                    </p>
-                      <div className={SheetStyles.sideContent}>
-                        <img className={SheetStyles.image} style={{position:'sticky',top:'calc(var(--navigation-height)+2px)'}} src={zoom_01}/>
-                      </div>
-                    <p className={SheetStyles.paragraph}>
-                      <span className="cite">
+                      <span className={SheetStyles.paragraph + 'cite'}>
                       "C’è, quasi nascosto dentro il libro, un altro libro più sommesso, di nostalgica evocazione d’ un paesaggio, o meglio: di ri - invenzione d’ un paesaggio attraverso la composizione, l’ ingrandimento, la moltiplicazione di sparsi elementi di memoria. [...] Il romanzo si svolge in un paese immaginario, Ombrosa, ma ci rendiamo presto conto che questa ombrosa si trova in un punto imprecisato della Riviera ligure.Dai dati biografici dell’ Autore sappiamo che egli è di San Remo, che nella cittadina ligure ha passato infanzia e giovinezza fino all’ immediato dopoguerra;
                       da altri scritti dell’ Autore il suo legame col paese risulta nutrito di memorie più antiche[...].
                       </span>
-                    </p>
-                    <p className={SheetStyles.paragraph}>
-                    Evocare il paesaggio ligure, che per Calvino è il paesaggio per eccellenza, assume allora una precisa finalità:
-                    </p>
-                    <p className={SheetStyles.paragraph}>
-                      <span className="cite">
-                      Ma tutto questo paesaggio geografico e ideale appartiene al passato: sappiamo che la Riviera in questo dopoguerra è diventata irriconoscibile per il modo caotico in cui si è riempita di caseggiati urbani fino a trasformarsi in una distesa di cemento;
-                      sappiamo che le speculazioni economiche e un facile edonismo dominano i rapporti umani di una larga parte della nostra società.Ed è solo da tutti questi elementi sommati insieme che possiamo ricavare la radice lirica del libro, la prima spinta dell’ invenzione poetica.Partendo da un mondo che non esiste più, l’ Autore regredisce a un mondo mai esistito che contenga i nuclei di ciò che è stato e di ciò che avrebbe potuto essere, le allegorie del passato e del presente, le interrogazioni sulla propria esperienza."
-                      </span>
-                  </p>
+                      </p>
+                      <div className={SheetStyles.sideContent}>
+                        <img className={SheetStyles.image} style={{position:'sticky', top:'calc(var(--navigation-height)+4em)'}} src={zoom_01}/>
+                      </div>
                   <p className={SheetStyles.paragraph}>
-                    Un discorso diverso, ma non così distante, può essere fatto per le Città invisibili, l’opera che raccoglie il maggior numero di ambientazioni fantastiche. Le varie Eufemia, Zora, Despina sembrano così rarefatte che è facile credere di trovarsi infine fuori dal tempo e dallo spazio. Invece, anche in questo caso, Calvino non rinuncia a una cornice storica, poiché tutte e cinquantacinque le città visitate da Marco Polo appartengono allo sterminato impero di Kublai Khan.
+                    Evocare il paesaggio ligure, che per Calvino è il paesaggio per eccellenza, assume allora una precisa finalità:
+                      <p><span className={SheetStyles.paragraph + 'cite'}>
+                      "Ma tutto questo paesaggio geografico e ideale appartiene al passato: sappiamo che la Riviera in questo dopoguerra è diventata irriconoscibile per il modo caotico in cui si è riempita di caseggiati urbani fino a trasformarsi in una distesa di cemento;
+                      sappiamo che le speculazioni economiche e un facile edonismo dominano i rapporti umani di una larga parte della nostra società.Ed è solo da tutti questi elementi sommati insieme che possiamo ricavare la radice lirica del libro, la prima spinta dell’ invenzione poetica.Partendo da un mondo che non esiste più, l’ Autore regredisce a un mondo mai esistito che contenga i nuclei di ciò che è stato e di ciò che avrebbe potuto essere, le allegorie del passato e del presente, le interrogazioni sulla propria esperienza."
+                      </span></p>
+                      <p className={SheetStyles.paragraph}>
+                      Un discorso diverso, ma non così distante, può essere fatto per le Città invisibili,
+                      l’opera che raccoglie il maggior numero di ambientazioni fantastiche.
+                      Le varie Eufemia, Zora, Despina sembrano così rarefatte che è facile credere di trovarsi infine fuori
+                      dal tempo e dallo spazio. Invece, anche in questo caso, Calvino non rinuncia a una cornice storica,
+                      poiché tutte e cinquantacinque le città visitate da Marco Polo appartengono allo sterminato impero di Kublai Khan.
+                      </p>
                   </p>
-                    <div className={SheetStyles.sideContent}>
-                      <img className={SheetStyles.image} style={{position:'sticky',top:'calc(var(--navigation-height)+2px)'}} src={zoom_02}/>
-                    </div>
+                      <div className={SheetStyles.sideContent}>
+                        <img className={SheetStyles.image} style={{position:'sticky', top:'calc(var(--navigation-height)+4em)'}} src={zoom_02}/>
+                      </div>
+
                   </div>
 
             <div className={SheetStyles.gridRow}>
               <h3 className={SheetStyles.subtitleSheet}>I racconti</h3>
-                    <p className={SheetStyles.paragraph}>
+                        <img className={SheetStyles.image} style={{gridColumn:'1 / span 7'}} src={zoom_04}/>
+                        <p className='captionLeft' style={{gridColumn:'1 / span 7'}} >Una selezione di racconti scritti a metà degli anni Cinquanta</p>
+                    <p className={SheetStyles.paragraphBig}>
                     Nella forma breve, sia che si tratti di racconti pubblicati in volumi sia che si tratti di testi scritti per i periodici e poi rimasti inediti,
                     la diffusione di luoghi inventati è decisamente inferiore.
                     Tuttavia, come mostra la visualizzazione, ci sono alcune interessanti zone di concentrazione,
                     una in particolare più rilevante delle altre.
-                        <img className={SheetStyles.image} src={zoom_04}/>
-                    </p>
-                    <p className={SheetStyles.paragraph}>
                     Tra la fine del 1953 e l’inizio del 1954, dopo aver pubblicato sull’«Unità» di Torino il primo blocco di storie che andranno a comporre Marcovaldo, Calvino scrive una serie di testi brevi di natura molto diversa, accomunati dall’incedere favolistico e dalla presenza di ambientazioni fantastiche. Due di questi compaiono ancora sull’edizione torinese dell’«Unità»; gli altri tre, scritti l’anno successivo, sono pubblicati sul settimanale «Il Contemporaneo» in una rubrica intitolata I viaggi di Gulliver all’interno della quale Calvino raccolse alcuni apologhi politici con riferimenti evidenti all’attualità.
                     Come evidenziato da Francesca Serra ciò che accomuna tutti questi testi è la presenza di un’ambientazione unidimensionale – un generico paese senza nome o luoghi d’invenzione come Sant’Alcide, Marzalia, Panduria, Cocinindia o Atrabilia – «dedita a una sola cosa, che sottostà a una sola regola, che conosce un solo modo d’essere». Nella quasi totalità dei casi questa situazione iniziale viene messa a dura prova da un accadimento inatteso, che scombina l’ordine delle cose e da cui si ricava spesso un insegnamento o una morale.
                     L’aspetto più interessante di questi racconti scritti a metà degli anni Cinquanta – e anche quello che balza subito all’occhio – è il fatto che sia possibile riconoscere in nuce, pertanto in forma acerba e a tratti elementare, alcuni elementi fondanti delle Città Invisibili: l’ambientazione unidimensionale, la regola o il carattere sui generis che connota la città, i nomi dei luoghi esclusivamente femminili. È significativo: pur senza arrivare a tracciare un legame consapevole tra questo gruppo di testi minori e l’opera che consacrerà Calvino nel 1972, si può comunque riconoscere, in filigrana, un percorso organico e continuativo, che affonda le radici in un tempo piuttosto lontano e trova il suo compimento negli anni Settanta del Novecento.
@@ -110,6 +110,7 @@ export default class SpaceInformationSheet extends React.Component
             </div>
 
             <div className={SheetStyles.gridRow}>
+            <h2 className={SheetStyles.titleH2}>Bibliografia</h2>
               <ol className={SheetStyles.paragraph+' '+SheetStyles.references}>
                   <li className={SheetStyles.referenceItem}>
                   Frank Zipfel, Fiktion, Fiktivität, Fiktionalität: Analysen zur Fiktion in der Literatur und zum Fiktionsbegriff in der
@@ -128,12 +129,12 @@ export default class SpaceInformationSheet extends React.Component
                    Alfonso Berardinelli, Calvino moralista. Ovvero restare sani dopo la fine del mondo, in «Diario», VII, 9, 1991, p. 39</li>
               </ol>
             </div>
-          
-            <div className={SheetStyles.gridRow}>
-              <div style={{gridColumn:'1 / span 3'}}><ArrowButton arrowDirection="none" textAlign="left" text="VISUALIZZAZIONE PRINCIPALE" route="/Process/transforming" /></div>
-              <div style={{gridColumn:'4 / span 2'}}><ArrowButton arrowDirection="left" textAlign="right" text="TAPPA 1" route="/Phenomena/territory/spaceAnalysis"/></div>
-              <div style={{gridColumn:'6 / span 2'}}><ArrowButton arrowDirection="right" textAlign="left" text="TAPPA 3" route="/Problem/realism" /></div>
-            </div>
+
+          <div className={SheetStyles.gridRow}>
+            <div style={{gridColumn:'1 / span 3'}}><ArrowButton arrowDirection="none" textAlign="left" text="ANALISI" route="/Process/transforming" /></div>
+            <div style={{gridColumn:'4 / span 2'}}><ArrowButton arrowDirection="left" textAlign="right" text="TAPPA 1" route="/Phenomena/territory/spaceAnalysis"/></div>
+            <div style={{gridColumn:'6 / span 2'}}><ArrowButton arrowDirection="right" textAlign="left" text="TAPPA 3" route="/Problem/realism" /></div>
+          </div>
         </main>
       </>
     );
