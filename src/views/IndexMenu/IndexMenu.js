37b6b2ab30d50d71e162e5505c112bfbfb815702
skaffo <matteo.scaffidi@gmail.com>
2020-10-16 14:34:36 +0200

    Adding modal for guide

--- src/views/IndexMenu/IndexMenu.js
+++ src/views/IndexMenu/IndexMenu.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import IndexMenuHeader from "../../headers/IndexMenuHeader"
 import { Link } from "react-router-dom"
 import styles from "./IndexMenu.module.css"
@@ -25,7 +25,9 @@ import { ReactComponent as TramaIcon } from "./icons/trama_blue.svg"
 import { ReactComponent as IconApprofondimento } from "./icons/icon_approfondimento.svg"
 import { ReactComponent as Orbite } from "./icons/orbite.svg"
 import { ReactComponent as Bussola } from "./icons/bussola.svg"
+import { ReactComponent as IconCloseGuida } from "../../headers/IndexMenuHeader/icons/icon-close.svg"
 import Footer from "../../headers/Footer/Footer"
+import { Modal } from "react-bootstrap"
 
 const ItemIndex = ({
   icon,
@@ -36,7 +38,7 @@ const ItemIndex = ({
   linkApprofondimento,
 }) => {
   return (
-    <div className={`${styles[className]} ${styles['hoverable-icon']}`}>
+    <div className={`${styles[className]} ${styles["hoverable-icon"]}`}>
       <Link to={link}>
         {icon}
         {iconHover}
@@ -52,14 +54,16 @@ const ItemIndex = ({
 }
 
 export default function IndexMenu() {
+  const [showGuida, setShowGuida] = useState(false)
+
+  const handleCloseGuida = () => setShowGuida(false)
+  const handleShowGuida = () => setShowGuida(true)
+
   return (
     <div>
       <IndexMenuHeader />
       <div className="position-absolute" style={{ top: 70, left: 100 }}>
-        <Bussola width='70' />
-      </div>
-      <div className={`position-absolute ${styles['guarda-la-guida']}`}>
-          Guarda la guida
+        <Bussola width="70" />
       </div>
       <div
         className="position-absolute"
@@ -70,35 +74,65 @@ export default function IndexMenu() {
       <div className="IndexMenu position-relative">
         <ItemIndex
           title="territorio"
-          link='Phenomena/territory'
+          link="Phenomena/territory"
           className={"territorio"}
-          icon={<TerritorioIcon className={`${styles["basic-icon"]} ${styles["icon-width"]}`} />}
-          iconHover={<TerritorioIconHover className={`${styles["hover-icon"]} ${styles["icon-width"]}`} />}
+          icon={
+            <TerritorioIcon
+              className={`${styles["basic-icon"]} ${styles["icon-width"]}`}
+            />
+          }
+          iconHover={
+            <TerritorioIconHover
+              className={`${styles["hover-icon"]} ${styles["icon-width"]}`}
+            />
+          }
         />
         <ItemIndex
           title="dubitare"
           link="Process/doubting"
           linkApprofondimento="Process/doubting/Hesitation/informationSheet"
           className={"dubitare"}
-          icon={<DubbioIcon className={`${styles["basic-icon"]} ${styles["icon-width"]}`} />}
-          iconHover={<DubbioIconHover className={`${styles["hover-icon"]} ${styles["icon-width"]}`} />}
+          icon={
+            <DubbioIcon
+              className={`${styles["basic-icon"]} ${styles["icon-width"]}`}
+            />
+          }
+          iconHover={
+            <DubbioIconHover
+              className={`${styles["hover-icon"]} ${styles["icon-width"]}`}
+            />
+          }
         />
         <ItemIndex
           title="nebbia"
-          linkApprofondimento='Phenomena/territory/doubtAnalysis/informationSheet'
+          linkApprofondimento="Phenomena/territory/doubtAnalysis/informationSheet"
           link="Phenomena/territory/doubtAnalysis"
           className={"nebbia"}
-          icon={<NebbiaIcon className={`${styles["basic-icon"]} ${styles["icon-width"]}`} />}
-          iconHover={<NebbiaIconHover className={`${styles["hover-icon"]} ${styles["icon-width"]}`} />}
+          icon={
+            <NebbiaIcon
+              className={`${styles["basic-icon"]} ${styles["icon-width"]}`}
+            />
+          }
+          iconHover={
+            <NebbiaIconHover
+              className={`${styles["hover-icon"]} ${styles["icon-width"]}`}
+            />
+          }
         />
         <ItemIndex
           title="cancellazione"
-          linkApprofondimento='cancellation-sheet'
+          linkApprofondimento="cancellation-sheet"
           link="Problem/cancellation"
           className={"cancellazione"}
-          icon={<CancellazioneIcon className={`${styles["basic-icon"]} ${styles["icon-width"]}`} />}
+          icon={
+            <CancellazioneIcon
+              className={`${styles["basic-icon"]} ${styles["icon-width"]}`}
+            />
+          }
           iconHover={
-            <CancellazioneIconHover className={`${styles["hover-icon"]} ${styles["icon-width"]}`} />
+            <CancellazioneIconHover
+              className={`${styles["hover-icon"]} ${styles["icon-width"]}`}
+            />
           }
         />
         <ItemIndex
@@ -106,58 +140,125 @@ export default function IndexMenu() {
           linkApprofondimento="Process/transforming/Transform/informationSheet"
           className={"trasformare"}
           link="Process/transforming"
-          icon={<TrasformareIcon className={`${styles["basic-icon"]} ${styles["icon-width"]}`} />}
-          iconHover={<TrasformareIconHover className={`${styles["hover-icon"]} ${styles["icon-width"]}`} />}
+          icon={
+            <TrasformareIcon
+              className={`${styles["basic-icon"]} ${styles["icon-width"]}`}
+            />
+          }
+          iconHover={
+            <TrasformareIconHover
+              className={`${styles["hover-icon"]} ${styles["icon-width"]}`}
+            />
+          }
         />
         <ItemIndex
           title="luoghi"
           linkApprofondimento="Phenomena/territory/spaceAnalysis/informationSheet"
           link="Phenomena/territory/spaceAnalysis"
           className={"luoghi"}
-          icon={<LuoghiIcon className={`${styles["basic-icon"]} ${styles["icon-width"]}`} />}
-          iconHover={<LuoghiIconHover className={`${styles["hover-icon"]} ${styles["icon-width"]}`} />}
+          icon={
+            <LuoghiIcon
+              className={`${styles["basic-icon"]} ${styles["icon-width"]}`}
+            />
+          }
+          iconHover={
+            <LuoghiIconHover
+              className={`${styles["hover-icon"]} ${styles["icon-width"]}`}
+            />
+          }
         />
         <ItemIndex
           title="realismo"
           linkApprofondimento="Process/combining/Combine/informationSheet"
           className={"realismo"}
           link="Problem/realism"
-          icon={<RealismoIcon className={`${styles["basic-icon"]} ${styles["icon-width"]}`} />}
-          iconHover={<RealismoIconHover className={`${styles["hover-icon"]} ${styles["icon-width"]}`} />}
+          icon={
+            <RealismoIcon
+              className={`${styles["basic-icon"]} ${styles["icon-width"]}`}
+            />
+          }
+          iconHover={
+            <RealismoIconHover
+              className={`${styles["hover-icon"]} ${styles["icon-width"]}`}
+            />
+          }
         />
         <ItemIndex
           title="elenchi"
           linkApprofondimento="Phenomena/territory/shapeAnalysis/informationSheet"
           link="Phenomena/territory/shapeAnalysis"
           className={"elenchi"}
-          icon={<ElenchiIcon className={`${styles["basic-icon"]} ${styles["icon-width"]}`} />}
-          iconHover={<ElenchiIconHover className={`${styles["hover-icon"]} ${styles["icon-width"]}`} />}
+          icon={
+            <ElenchiIcon
+              className={`${styles["basic-icon"]} ${styles["icon-width"]}`}
+            />
+          }
+          iconHover={
+            <ElenchiIconHover
+              className={`${styles["hover-icon"]} ${styles["icon-width"]}`}
+            />
+          }
         />
         <ItemIndex
           title="combinare"
           link="Process/combining"
           className={"combinare"}
-          icon={<CombinareIcon className={`${styles["basic-icon"]} ${styles["icon-width"]}`} />}
-          iconHover={<CombinareIconHover className={`${styles["hover-icon"]} ${styles["icon-width"]}`} />}
+          icon={
+            <CombinareIcon
+              className={`${styles["basic-icon"]} ${styles["icon-width"]}`}
+            />
+          }
+          iconHover={
+            <CombinareIconHover
+              className={`${styles["hover-icon"]} ${styles["icon-width"]}`}
+            />
+          }
         />
         <ItemIndex
           title="trama"
           link="Problem/plot"
           className={"trama"}
-          icon={<TramaIcon className={`${styles["basic-icon"]} ${styles["icon-width"]}`} />}
-          iconHover={<TramaIconHover className={`${styles["hover-icon"]} ${styles["icon-width"]}`} />}
+          icon={
+            <TramaIcon
+              className={`${styles["basic-icon"]} ${styles["icon-width"]}`}
+            />
+          }
+          iconHover={
+            <TramaIconHover
+              className={`${styles["hover-icon"]} ${styles["icon-width"]}`}
+            />
+          }
         />
         <div className={styles.spazio}>
-          <div className={styles['big-name']}>Spazio</div>
+          <div className={styles["big-name"]}>Spazio</div>
         </div>
         <div className={styles.forma}>
-          <div className={styles['big-name']}>Forma</div>
+          <div className={styles["big-name"]}>Forma</div>
         </div>
         <div className={styles.dubbio}>
-          <div className={styles['big-name']}>Dubbio</div>
+          <div className={styles["big-name"]}>Dubbio</div>
         </div>
       </div>
+      <div
+        className={`position-absolute cursor-pointer ${styles["guarda-la-guida"]}`}
+        onClick={handleShowGuida}
+      >
+        Guarda la guida
+      </div>
       <Footer />
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={showGuida}
+        onHide={handleCloseGuida}
+      >
+        <Modal.Body style={{ height: 500}}>
+          <div onClick={handleCloseGuida} className='text-right cursor-pointer'>
+            <IconCloseGuida />
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
