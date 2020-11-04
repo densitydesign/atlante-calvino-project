ae04b1cbd53e2b0ec256d2376446ff39cc39f79c
skaffo <matteo.scaffidi@gmail.com>
2020-11-04 16:13:14 +0100

    Adding header to translate

--- src/headers/IndexMenuHeader/IndexMenuHeader.js
+++ src/headers/IndexMenuHeader/IndexMenuHeader.js
@@ -1,59 +1,62 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import MainMenu from '../../general/MainMenu/MainMenu'
-import { ReactComponent as IconClose } from './icons/icon-close.svg'
+import React from "react"
+import { Link, NavLink } from "react-router-dom"
+import { useTranslation, Trans } from "react-i18next"
+import MainMenu from "../../general/MainMenu/MainMenu"
+import { ReactComponent as IconClose } from "./icons/icon-close.svg"
 
 export default function IndexMenuHeader({
   // 'openIndex' | 'closeIndex' | 'homeLink'
-  menuAction = 'openIndex',
+  menuAction = "openIndex",
   onClose,
 }) {
+  const { t } = useTranslation("translation")
+
   return (
     <div className="top-nav navigations">
-      {menuAction === 'openIndex' && <MainMenu />}
-      {menuAction === 'closeIndex' && (
+      {menuAction === "openIndex" && <MainMenu />}
+      {menuAction === "closeIndex" && (
         <div
           className="d-flex justify-content-center cursor-pointer"
-          style={{ gridColumn: 'span 1' }}
+          style={{ gridColumn: "span 1" }}
           onClick={() => onClose()}
         >
           <IconClose />
         </div>
       )}
-      {menuAction === 'homeLink' && (
+      {menuAction === "homeLink" && (
         <Link
           to="/"
           className="d-flex justify-content-center cursor-pointer align-items-center"
-          style={{ gridColumn: 'span 1' }}
+          style={{ gridColumn: "span 1" }}
         >
           <IconClose />
         </Link>
       )}
       <div
-        style={{ gridColumn: 'span 22' }}
+        style={{ gridColumn: "span 22" }}
         className="d-flex justify-content-center"
       >
         <NavLink to="/" className="ml-5 mr-5 top-menu">
           Atlante Calvino
         </NavLink>
         <NavLink to="/equipe" className="ml-5 mr-5 top-menu">
-          Équipe
+          {t("header.equipe")}
         </NavLink>
         <NavLink to="/project" className="ml-5 mr-5 top-menu">
-          Progetto
+          {t("header.progetto")}
         </NavLink>
         <NavLink to="/instructions" className="ml-5 mr-5 top-menu">
-          Istruzioni per l'uso
+          {t("header.istruzioni_per_uso")}
         </NavLink>
         <NavLink to="/capta" className="ml-5 mr-5 top-menu">
-          Capta
+          {t("header.capta")}
         </NavLink>
         <NavLink to="/publications" className="ml-5 mr-5 top-menu">
-          Pubblicazioni
+          {t("header.pubblicazioni")}
         </NavLink>
       </div>
       <div
-        style={{ gridColumn: 'span 1' }}
+        style={{ gridColumn: "span 1" }}
         className="d-flex align-items-center justify-content-center"
       >
         <Link className="text-dark-blue" to="/">
