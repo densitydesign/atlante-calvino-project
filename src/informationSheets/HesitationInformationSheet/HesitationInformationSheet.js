7d18c0f3f0e566797a06413724abee463c69a32d
Beatrice Gobbo <gobbo.beatrice92@gmail.com>
2020-11-08 20:26:20 +0100

    end hesitation

--- src/informationSheets/HesitationInformationSheet/HesitationInformationSheet.js
+++ src/informationSheets/HesitationInformationSheet/HesitationInformationSheet.js
@@ -61,22 +61,7 @@ class HesitationInformationSheet extends Component {
         </div>
 
         <div className={SheetStyles.gridRow}>
-          <h2 className={SheetStyles.titleH2}>Parole: istruzioni per l'uso</h2>
-          <p className={SheetStyles.paragraph}>
-          All’inizio del secondo capitolo, la voce narrante riflette su quale sia il modo migliore di «chiamare le cose» di cui si vuole discorrere: l’obiettivo è di raggiungere una forma di espressione esatta, precisa, che non generi alcuna incomprensione nella lettrice/lettore. La scelta ricade sui «termini generici», ritenuti più adatti rispetto ai termini specifici. Ognuno, infatti, è «portato ad attribuire valori diversi o magari contrastanti» a questi ultimi, «secondo le proprie cognizioni ed esperienze», e per questo motivo «resterebbe da precisare ancora» (RR, II, 7).</p>
-          <p className={SheetStyles.paragraph}>La riflessione che inaugura il capitolo è immediatamente coinvolta dal processo dubitativo. Con la promessa di una maggiore esattezza,
-          <span style={{textDecoration:'underline'}}>td7</span> e <span style={{textDecoration:'underline'}}>td8</span> cercano di giustificare
-          la scelta di impiegare solo «termini generici», evitando i nomi propri. Subito però il testo infrange questa regola di comportamento:
-          viene infatti svelata l’identità del «partito di sinistra» a cui appartiene il protagonista (ovvero «partito comunista») e l’«istituto
-          religioso» anonimo diventa «il Cottolengo di Torino».
-          Ci troviamo di fronte a una figura retorica,
-          la <span style={{textDecoration:'underline'}}>preterizione</span>,
-          che spesso collabora (non a caso) con il processo dubitativo: informare negando infatti è in linea con il funzionamento
-          del testo dubitativo, che alimenta la narrazione tramite un’operazione tutta volta alla sottrazione, ossia mettendo in discussione
-          quanto detto in precedenza.
-          </p>
-
-          <h2 className={SheetStyles.titleH2}>Parole: scegliere, interpretare, precisare </h2>
+      <h2 className={SheetStyles.titleH2}>Parole: scegliere, interpretare, precisare </h2>
           <p className={SheetStyles.paragraph}>
           All’inizio del secondo capitolo, la voce narrante riflette su quale sia il
           modo migliore di «chiamare le cose» di cui si vuole discorrere: l’obiettivo è
@@ -132,85 +117,77 @@ class HesitationInformationSheet extends Component {
           sviluppi laterali, a volte racchiusi dentro appositi segni grafici.</p>
 
           <img className={SheetStyles.sideContent} src={info_01} style={{position:'sticky',top:'calc(var(--navigation-height) + 3px)',gridColumn:'8 / span 3'}} />
-</div>
-
-        <div className={SheetStyles.gridRow}>
-        <h2 className={SheetStyles.titleH2}>Gli strati del testo</h2>
-            <div className={SheetStyles.paragraph}>
-            <p>Il paragrafo finale di questa parte del libro (RR, II, 9-11) è composto da più
-            di due pagine senza punti fermi: si tratta di una serie di riflessioni, che si
-            incastrano e talvolta sovrappongono. L’argomento principale è la fede politica
-            del protagonista. Dopo aver esaminato ogni elemento in grado di stabilirne
-            l’origine e le motivazioni profonde, producendo un testo denso e ricco di
-            informazioni, il filo del discorso si interrompe bruscamente poiché il narratore
-            afferma di «aver capito finalmente quel che non ci voleva poi tanto a capire»
-            (RR, II, 10). Questo paragrafo è un ottimo esempio di come il processo
-            dubitativo consista nel “tornare sui propri passi”. Il testo oggetto di dubbio
-            associato a td28 coincide con l’inizio del paragrafo e racchiude in sé dodici
-            occorrenze del testo dubitativo (dal <strong>td16</strong> al
-            <strong>td27</strong>, più della metà del numero totale di occorrenze del
-            capitolo). L’improvviso incremento di occorrenze non è unicamente numerico; in
-            parallelo assistiamo a un innalzamento di livello: il numero di livello equivale
-            alla quantità di volte in cui il processo dubitativo insiste sulla stessa
-            identica porzione di testo. Accavallandosi fra loro, le occorrenze dubitano
-            della loro capacità di ristabilire il senso del discorso. Un altro esempio: il
-            passaggio dal 6° al 7° livello avviene a causa del td29; poteva sembrare che il
-            testo (<strong>td28</strong>) fosse riuscito una volta per tutte a bloccare la
-            catena dubitativa, eppure ancora una volta c’è qualcosa da dire, da aggiungere,
-            da precisare. Ed è significativo che la precisazione avvenga fra parentesi.
-            Cinque delle occorrenze “intrappolate” dentro il testo oggetto di dubbio di <strong>td28</strong>
-            sono a loro volta contenute fra parentesi (<strong>td18</strong>,
-            <strong>td20</strong>, <strong>td21</strong>, <strong>td23</strong>,
-            <strong>td27</strong>). Ma la quantità di segni grafici nel paragrafo è molto
-            più alta: infatti circa il 30% del capitolo viene dislocata tra parentesi o
-            dentro un inciso, anche quando da un punto di vista narrativo non sembrerebbe
-            necessario. In alcuni casi le parentesi contengono a loro volta degli incisi,
-            elaborando così una sorta di racconto a matrioska. La massiccia concentrazione
-            del processo dubitativo proprio in quell’area è collegata alla presenza dei
-            segni grafici che stratificano il testo.</p>
-
-            <h2 className={SheetStyles.titleH2}>Il processo: i livelli del testo dubitativo</h2>
-
-          <div>
-            <img className={SheetStyles.sideContent} src={zoom_02} style={{position:'sticky',top:'calc(var(--navigation-height) + 1rem)', background:'#fffffa', width:'100%', paddingBottom:'0.5rem'}} />
-          <p>Questo paragrafo è un ottimo esempio di come il processo dubitativo consista nel “tornare sui propri passi”. Il testo oggetto di dubbio associato a td28 coincide con l’inizio del paragrafo e racchiude in sé dodici td (dal td16 al td27, più della metà del numero totale di occorrenze del capitolo). L’improvviso incremento di occorrenze non è unicamente numerico; in parallelo assistiamo a un innalzamento di livello: il numero di livello equivale alla quantità di volte in cui il processo dubitativo insiste sulla stessa identica porzione di testo. Accavallandosi fra loro le occorrenze “dubitano” della loro capacità di ristabilire il senso del discorso. Un altro esempio: il passaggio dal 6° al 7° livello avviene a causa del td29; poteva sembrare che il td28 fosse riuscito una volta per tutte a bloccare la catena dubitativa, eppure ancora una volta c’è qualcosa da dire, da aggiungere, da precisare. E la precisazione non può che avvenire fra parentesi.</p>
-          </div>
-          </div>
-          </div>
-
-          <div className={SheetStyles.gridRow}>
-            <h3 className={SheetStyles.subtitleSheet}>La forma: le parentesi</h3>
-            <h4 className={SheetStyles.titleH4}>Capitolo 2</h4>
-            </div>
-
-            <div className={SheetStyles.gridRow}>
-          <img className={SheetStyles.legend} style = {{gridColumn:' 1 / span 6', float:'left'}}  src={legenda_03}/>
-          <img className = {SheetStyles.image100w} style={{gridColumn:'1 / span 22'}} src={zoom_03}/>
         </div>
 
-            <div className={SheetStyles.gridRow}>
-            <div className={SheetStyles.paragraph}>
-            <p>Cinque delle occorrenze “intrappolate” dentro il testo oggetto di dubbio di td28 sono a loro volta contenute fra parentesi: td18, td20, td21, td23, td27. Ma la quantità di segni grafici nel paragrafo è molto più alta: infatti circa il 30% del capitolo viene dislocata tra parentesi o dentro un inciso, anche quando da un punto di vista narrativo non sembrerebbe necessario. In alcuni casi le parentesi contengono a loro volta degli incisi, elaborando così una sorta di racconto a “matrioska”. La massiccia concentrazione del processo dubitativo proprio in quell’area è collegata alla presenza dei segni grafici che “stratificano” il testo.
-            </p>
+        <div className={SheetStyles.gridRow}>
+          <h2 className={SheetStyles.titleH2}>Gli strati del testo</h2>
+          <img className = {SheetStyles.image} style ={{gridColumn:"1 / span 8", position:'sticky',top:'calc(var(--navigation-height) + 0.3rem)', backgroundColor:'var(--white)'}} src={zoom_02}/>
+          <p className={SheetStyles.paragraph}>
+              Il paragrafo finale di questa parte del libro (<em>RR</em>, II, 9-11) è composto da più
+              di due pagine senza punti fermi: si tratta di una serie di riflessioni, che si
+              incastrano e talvolta sovrappongono. L’argomento principale è la fede politica
+              del protagonista. Dopo aver esaminato ogni elemento in grado di stabilirne
+              l’origine e le motivazioni profonde, producendo un testo denso e ricco di
+              informazioni, il filo del discorso si interrompe bruscamente poiché il narratore
+              afferma di «aver capito finalmente quel che non ci voleva poi tanto a capire»
+              (<em>RR</em>, II, 10).
+              < br/>
+              Questo paragrafo è un ottimo esempio di come il processo
+              dubitativo consista nel “tornare sui propri passi”. Il testo oggetto di dubbio
+              associato a td28 coincide con l’inizio del paragrafo e racchiude in sé dodici
+              occorrenze del testo dubitativo (dal <strong>td16</strong> al
+              <strong> td27</strong>, più della metà del numero totale di occorrenze del
+              capitolo). L’improvviso incremento di occorrenze non è unicamente numerico; in
+              parallelo assistiamo a un innalzamento di livello: il numero di livello equivale
+              alla quantità di volte in cui il processo dubitativo insiste sulla stessa
+              identica porzione di testo. Accavallandosi fra loro, le occorrenze dubitano
+              della loro capacità di ristabilire il senso del discorso. Un altro esempio: il
+              passaggio dal 6° al 7° livello avviene a causa del <strong>td29</strong>; poteva sembrare che il
+              testo (<strong>td28</strong>) fosse riuscito una volta per tutte a bloccare la
+              catena dubitativa, eppure ancora una volta c’è qualcosa da dire, da aggiungere,
+              da precisare. Ed è significativo che la precisazione avvenga fra parentesi.
+              < br/>
+              Cinque delle occorrenze “intrappolate” dentro il testo oggetto di dubbio di <strong>td28 </strong>
+              sono a loro volta contenute fra parentesi (<strong>td18</strong>,
+              <strong> td20</strong>, <strong>td21</strong>, <strong>td23</strong>,
+               <strong> td27</strong>). Ma la quantità di segni grafici nel paragrafo è molto
+              più alta: infatti circa il 30% del capitolo viene dislocata tra parentesi o
+              dentro un inciso, anche quando da un punto di vista narrativo non sembrerebbe
+              necessario. In alcuni casi le parentesi contengono a loro volta degli incisi,
+              elaborando così una sorta di racconto a matrioska. La massiccia concentrazione
+              del processo dubitativo proprio in quell’area è collegata alla presenza dei
+              segni grafici che stratificano il testo.</p>
+              </div>
 
-            <h2 className={SheetStyles.titleH2}>Conclusione</h2>
-            <p>Alla fine del secondo capitolo della Giornata d’uno scrutatore, il testo si sviluppa su piani diversi, i quali a volte collaborano, a volte confliggono. La struttura a “strati” sovrapposti, come le «foglie d’un carciofo», da una parte si sforza di garantire maggiore chiarezza al lettore/lettrice, dall’altra li disorienta. Si tratta di un meccanismo narrativo basato sul difficile rapporto fra la ricerca dell’esattezza della scrittura e il fallimento dell’atto interpretativo, per cui l’unico modo per parlare della «complessità delle cose» sembra essere quello di organizzarla in strati (ibidem).
-            La parentesi e l’inciso generano questa stratificazione,
-            attraverso una forma grafica che introduce un “altro” spazio,
-            un altrove rispetto al discorso principale. Tale sdoppiamento del testo viene ulteriormente potenziato dal processo dubitativo,
-            che rinforza l’effetto di stratificazione in una modalità meno visibile.</p>
-            </div>
-          </div>
+              <div className={SheetStyles.gridRow}>
+
+              <img className={SheetStyles.legend} style = {{gridColumn:' 1 / span 6', float:'left'}}  src={legenda_03}/>
+              <img className = {SheetStyles.image100w} src={zoom_03}/>
+              <p className={SheetStyles.paragraph}>Alla fine del secondo capitolo della <em>Giornata d’uno scrutatore</em>, il testo si
+              sviluppa su piani diversi, i quali a volte collaborano, a volte confliggono. La
+              struttura a strati sovrapposti, come le «foglie d’un carciofo» (<em>RR</em>, II, 9), da
+              una parte si sforza di garantire maggiore chiarezza alla lettrice/lettore,
+              dall’altra li disorienta. Si tratta di un meccanismo narrativo basato sul
+              difficile rapporto fra la ricerca dell’esattezza della scrittura e l’insuccesso
+              dell’atto interpretativo, per cui l’unico modo per parlare della «complessità
+              delle cose» (<em>ibidem</em>) sembra essere quello di organizzarla in strati.
+              < br/>
+              La parentesi e l’inciso generano questa stratificazione, attraverso una forma
+              grafica che introduce un altro spazio, un altrove rispetto al discorso
+              principale. Tale sdoppiamento del testo viene ulteriormente potenziato dal
+              processo dubitativo, che rinforza l’effetto di stratificazione in una modalità
+              meno visibile.</p>
+              </div>
 
 
           <div className={SheetStyles.gridRow}>
           <h2 className={SheetStyles.titleH2}>Bibliografia</h2>
             <ol className={[SheetStyles.paragraph,'references'].join(' ')  }>
+            <li className="referenceItem">
+            Barenghi 2009: M. Barenghi, <em>Calvino</em>, Bologna, Mulino.
+            </li>
               <li className="referenceItem">
-              Mengaldo 1996: P.V. Mengaldo, Aspetti della lingua di Calvino, in Id., La tradizione del Novecento, Torino, Einaudi, pp. 227-291.
-              </li>
-              <li className="referenceItem">
-              Barenghi 2009: M. Barenghi, Calvino, Bologna, Mulino.
+              Mengaldo 1996: P.V. Mengaldo, <em>Aspetti della lingua di Calvino</em>, in Id., <em>La tradizione del Novecento</em>, Torino, Einaudi, pp. 227-291.
               </li>
           </ol>
           </div>
