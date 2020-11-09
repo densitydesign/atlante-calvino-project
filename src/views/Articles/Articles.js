10c84f3b4681e9a158cb9f241e3210fc902ac1cb
Beatrice Gobbo <gobbo.beatrice92@gmail.com>
2020-11-09 18:55:49 +0100

    grid publications

--- src/views/Articles/Articles.js
+++ src/views/Articles/Articles.js
@@ -1,74 +1,112 @@
+import React from "react";
 
-import React from 'react';
+import * as d3 from "d3";
+import Footer from "../../headers/Footer/Footer";
 
-import * as d3 from 'd3';
-import Footer from '../../headers/Footer/Footer';
+import IndexMenuHeader from "../../headers/IndexMenuHeader";
 
-import '../../general/GridsWithScrollableColumns/GridWithScrollableColumn.css';
-import '../../general/GridsWithScrollableColumns/ScrollableColumn.css';
-import IndexMenuHeader from '../../headers/IndexMenuHeader';
-
-export default class Articles extends React.Component
-{
-  constructor(props){
+export default class Articles extends React.Component {
+  constructor(props) {
     super(props);
 
     this.state = {
-      data: []
-    }
+      data: [],
+    };
   }
 
   componentDidMount() {
-    console.log('press review')
-    d3.tsv(process.env.PUBLIC_URL+'/pubblicazioni.tsv').then(data => {
-      this.setState({data: data})
-    })
+    console.log("press review");
+    d3.tsv(process.env.PUBLIC_URL + "/pubblicazioni.tsv").then((data) => {
+      this.setState({ data: data });
+    });
   }
 
-  render()
-  {
-    console.log(this.state)
+  render() {
+    console.log(this.state);
     return (
       <>
         <IndexMenuHeader />
-        <div className="grid-with-scrollable-column">
-          <div className="scrollable-column-3 col-md-12 col-lg-12">
+        <div className="ac-grid-24">
+          <div className="content">
             <h1>Pubblicazioni</h1>
             <h2>Rassegna stampa</h2>
-            <span id="overlay"><img src={process.env.PUBLIC_URL + '/imagesPop/visconte-orange.jpg'} className="sizein" alt="information complement" style={{"margin-left":"2em"}} /></span>
-            {
-              this.state.data.filter(d=>d.section==="Articoli").map((d,i)=>{
+            {this.state.data
+              .filter((d) => d.section === "Articoli")
+              .map((d, i) => {
                 let externalLinkLink;
-                if (d.link!=='') {
-                  externalLinkLink = (<><a className="link" href={d.link} target="_blank" rel="noopener noreferrer">Link esterno</a></>);
+                if (d.link !== "") {
+                  externalLinkLink = (
+                    <>
+                      <a
+                        className="link"
+                        href={d.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Link esterno
+                      </a>
+                    </>
+                  );
                 }
                 let archiveLink;
-                if (d.archive!=='') {
-                  archiveLink = (<>, <a className="link" href={d.archive} target="_blank" rel="noopener noreferrer">Versione archiviata</a></>);
+                if (d.archive !== "") {
+                  archiveLink = (
+                    <>
+                      ,{" "}
+                      <a
+                        className="link"
+                        href={d.archive}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Versione archiviata
+                      </a>
+                    </>
+                  );
                 }
                 let download;
-                if (d.download!=='') {
-                  download = (<>, <a a className="link" href={d.download} target="_blank" rel="noopener noreferrer">PDF</a></>);
+                if (d.download !== "") {
+                  download = (
+                    <>
+                      ,{" "}
+                      <a
+                        a
+                        className="link"
+                        href={d.download}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        PDF
+                      </a>
+                    </>
+                  );
                 }
 
                 let pages;
-                if (d.pages!=='') {
-                  pages = (<>{d.pages}, </>);
+                if (d.pages !== "") {
+                  pages = <>{d.pages}, </>;
                 }
 
                 return (
                   <div className="pubblication-item" key={i}>
                     <h3>{d.title}</h3>
-                    <h5>{d.venue}, {pages}{d.date}</h5>
+                    <h5>
+                      {d.venue}, {pages}
+                      {d.date}
+                    </h5>
                     <h5>{d.authors}</h5>
                     <p className="pubblication-abstract">{d.abstract}</p>
-                    <h5>{externalLinkLink}{archiveLink}{download}</h5>
+                    <h5>
+                      {externalLinkLink}
+                      {archiveLink}
+                      {download}
+                    </h5>
                   </div>
-                )
-              })
-            }
+                );
+              })}
+          </div>
         </div>
-        </div>
+
         <Footer />
       </>
     );
--- src/views/PressReview/PressReview.js
+++ src/views/PressReview/PressReview.js
@@ -1,65 +1,103 @@
+import React from "react";
+import IndexMenuHeader from "../../headers/IndexMenuHeader";
 
-import React from 'react';
+import * as d3 from "d3";
 
-import * as d3 from 'd3';
+import HamburgerIntroHeader from "../../headers/HamburgerIntroHeader/HamburgerIntroHeader";
+import Footer from "../../headers/Footer/Footer";
 
-import HamburgerIntroHeader from '../../headers/HamburgerIntroHeader/HamburgerIntroHeader';
-import Footer from '../../headers/Footer/Footer';
-
-import '../../general/GridsWithScrollableColumns/GridWithScrollableColumn.css';
-import '../../general/GridsWithScrollableColumns/ScrollableColumn.css';
-
-export default class PressReview extends React.Component
-{
-  constructor(props){
+export default class PressReview extends React.Component {
+  constructor(props) {
     super(props);
 
     this.state = {
-      data: []
-    }
+      data: [],
+    };
   }
 
   componentDidMount() {
-    console.log('press review')
-    d3.tsv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQTLGjZLV0-w_72af5IdeY9kNUA6FTqjWdGfR1LGfLz5ix5CusACEWo0jB77hHdKRM2hPegDYawAoXo/pub?output=tsv').then(data => {
-      this.setState({data: data})
-    })
+    console.log("press review");
+    d3.tsv(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vQTLGjZLV0-w_72af5IdeY9kNUA6FTqjWdGfR1LGfLz5ix5CusACEWo0jB77hHdKRM2hPegDYawAoXo/pub?output=tsv"
+    ).then((data) => {
+      this.setState({ data: data });
+    });
   }
 
-  render()
-  {
+  render() {
     return (
       <>
         <HamburgerIntroHeader />
-        <div className="grid-with-scrollable-column">
-          <div className="scrollable-column-3 col-md-12 col-lg-8">
+        <div className="ac-grid-24">
+          <div className="content">
             <h1>RASSEGNA STAMPA</h1>
-            {
-              this.state.data.filter(d=>d.section==="Rassegna").map((d,i)=>{
+            {this.state.data
+              .filter((d) => d.section === "Rassegna")
+              .map((d, i) => {
                 let externalLink;
-                if (d.link!=='') {
-                  externalLink = (<><a className="link" href={d.link} target="_blank" rel="noopener noreferrer">Link esterno</a></>);
+                if (d.link !== "") {
+                  externalLink = (
+                    <>
+                      <a
+                        className="link"
+                        href={d.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Link esterno
+                      </a>
+                    </>
+                  );
                 }
                 let archiveLink;
-                if (d.archive!=='') {
-                  archiveLink = (<>, <a href={d.archive} className="link" target="_blank" rel="noopener noreferrer">Versione archiviata</a></>);
+                if (d.archive !== "") {
+                  archiveLink = (
+                    <>
+                      ,{" "}
+                      <a
+                        href={d.archive}
+                        className="link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Versione archiviata
+                      </a>
+                    </>
+                  );
                 }
                 let download;
-                if (d.download!=='') {
-                  download = (<>, <a href={d.download} className="link" target="_blank" rel="noopener noreferrer">PDF</a></>);
+                if (d.download !== "") {
+                  download = (
+                    <>
+                      ,{" "}
+                      <a
+                        href={d.download}
+                        className="link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        PDF
+                      </a>
+                    </>
+                  );
                 }
                 return (
                   <div className="pubblication-item" key={i}>
                     <h3>{d.title}</h3>
-                    <p>{d.venue}, {d.date}, {d.pages}</p>
-                    <h5>{externalLink}{archiveLink}{download}</h5>
+                    <p>
+                      {d.venue}, {d.date}, {d.pages}
+                    </p>
+                    <h5>
+                      {externalLink}
+                      {archiveLink}
+                      {download}
+                    </h5>
                   </div>
-                )
-              })
-            }
-
+                );
+              })}
           </div>
         </div>
+
         <Footer />
       </>
     );
