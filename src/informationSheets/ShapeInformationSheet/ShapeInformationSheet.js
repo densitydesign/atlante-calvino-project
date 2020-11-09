a38b6dee8c95ed4c5f9fba5c5f4c190dc9b38e5c
Beatrice Gobbo <gobbo.beatrice92@gmail.com>
2020-11-09 09:50:51 +0100

    Update ShapeInformationSheet.js

--- src/informationSheets/ShapeInformationSheet/ShapeInformationSheet.js
+++ src/informationSheets/ShapeInformationSheet/ShapeInformationSheet.js
@@ -63,7 +63,7 @@ export default class ShapeInformationSheet extends React.Component
       che il picco più alto del grafico corrisponda al 1945, anno in cui Calvino
       pubblica un solo racconto: <em>Angoscia in caserma</em>. Il testo in questione,
       infatti, è composto dalle riflessioni di un partigiano che, durante
-      un’esperienza di prigionia, viene colto dal «male dei simboli» (RR, I, 237)
+      un’esperienza di prigionia, viene colto dal «male dei simboli» (<em>RR</em>, I, 237)
       vale a dire dall’ossessione di vedere oscuri segnali premonitori in tutti i
       singoli dettagli dell’ambiente circostante, che vengono isolati dal contesto e
       riempiti di un eccesso di significato che finisce per cancellare la loro
@@ -79,8 +79,38 @@ export default class ShapeInformationSheet extends React.Component
       </div>
 
       <h2 className={SheetStyles.titleH2}> Parole 1965</h2>
-      <p className={SheetStyles.paragraph}>Tra le variazioni diacroniche che interessano le proporzioni tra le diverse tipologie elencatorie usate da Calvino, spicca nel 1965 una presenza particolarmente alta di elenchi di singole parole. Questa modalità, che nello spettro della figura retorica dell’enumerazione coincide pienamente con il polo dell’accumulazione,(<em>A. Marchese, Dizionario di retorica e stilistica, Mondadori, Milano 1991</em>) erode ogni residuo di complessità sintattica riducendo gli elementi della serie a unità grammaticali di base, e quindi porta sulla pagina il massimo effetto di frammentazione e caos. Nella storia letteraria occidentale, proprio questo particolare tipo di elenco subisce un ribaltamento totale di significato a partire dal XVI secolo, quando le forme simboliche medievali cominciano a trasformarsi nell’estetica della modernità: l’accumulazione passa dal rappresentare un omaggio all’organicità divina del creato ("Dio possiede il catalogo dell’universo e il primo privilegio che assegna ad Adamo è non a caso quello di dare un nome agli animali dell’Eden") al configurare un mondo abbandonato dall’ordine divino, in cui «le cose divengono autonome e iniziano ad accalcarsi intorno all’uomo, mischiandosi con le creature, con l’uomo stesso, con i suoi utensili, le sue idee, i sentimenti e perfino le parole».<em>L. Spitzer, L’enumerazione caotica nella poesia moderna, in «L’Asino d’Oro», 3, II, 1991</em>).
-      Un passaggio che genera la tendenza opposta, tipica ad esempio del Barocco, per cui «quanto più le cose – e le parole – tendono ad emanciparsi, tanto più le forze coercitive aumentano di numero, per farle ritornare al corrispondente stato naturale». Diventa allora interessante notare che il 1965 è l’anno che Calvino dedica interamente a costruire la galassia delle «storie cosmicomiche» (scrive 8 dei 12 testi del volume <em>Le cosmicomiche</em>, uscito l’anno stesso, e 3 dei testi che confluiranno nella “raccolta sorella” <em>La memoria del mondo</em> del 1968): testi in cui una fortissima cornice concettuale, ancorata direttamente alle scienze dure, viene usata per imbrigliare storie dominate da eventi dispersivi e forze centrifughe, presenti sia sul piano dei contenuti sia su quello della forma. Non è un caso che simili incrementi nell’uso di questa modalità elencatoria corrispondano a due opere che condividono questa impostazione: <em>Le città invisibili</em> e <em>Palomar</em>.
+      <p className={SheetStyles.paragraph}>Tra le variazioni diacroniche che
+      interessano le proporzioni tra le diverse tipologie elencatorie usate da
+      Calvino, spicca nel 1965 una presenza particolarmente alta di elenchi di
+      singole <span style={{color:'#00c19c'}}>parole</span>.
+      Questa modalità, che nello spettro della figura retorica dell’enumerazione
+      coincide pienamente con il polo dell’<em>accumulatio</em> (Marchese 1991, s.v.) erode
+      ogni residuo di complessità sintattica riducendo gli elementi della serie a
+      unità grammaticali di base, e quindi porta sulla pagina il massimo effetto di
+      frammentazione e caos. Nella storia letteraria occidentale, proprio questo
+      particolare tipo di elenco subisce un ribaltamento totale di significato a
+      partire dal XVI secolo, quando le forme simboliche medievali cominciano a
+      trasformarsi nell’estetica della modernità: l’accumulazione passa dal
+      rappresentare un omaggio all’organicità divina del creato (Dio possiede il
+      catalogo dell’universo e il primo privilegio che assegna ad Adamo è non a caso
+      quello di dare un nome agli animali dell’Eden) al configurare un mondo
+      abbandonato dall’ordine divino, in cui «le cose divengono autonome e iniziano ad
+      accalcarsi intorno all’uomo, mischiandosi con le creature, con l’uomo stesso,
+      con i suoi utensili, le sue idee, i sentimenti e perfino le parole» (Spitzer
+      1991, 123). Un passaggio che genera la tendenza opposta, tipica ad esempio del
+      Barocco, per cui «quanto più le cose – e le parole – tendono ad emanciparsi,
+      tanto più le forze coercitive aumentano di numero, per farle ritornare al
+      corrispondente stato naturale» (ivi, 109). Diventa allora interessante notare
+      che il 1965 è l’anno che Calvino dedica interamente a costruire la galassia
+      delle “storie cosmicomiche” (scrive 8 dei 12 testi del volume <em>Le cosmicomiche</em>,
+      uscito l’anno stesso, e 3 dei testi che confluiranno nella raccolta sorella <em>La
+      memoria del mondo</em> del 1968): testi in cui una fortissima cornice concettuale,
+      ancorata direttamente alle scienze dure, viene usata per imbrigliare storie
+      dominate da eventi dispersivi e forze centrifughe, presenti sia sul piano dei
+      contenuti sia su quello della forma. Non è un caso che simili incrementi
+      nell’uso di questa modalità elencatoria corrispondano a due opere che
+      condividono questa impostazione: <em>Le città invisibili</em> e <em>Palomar</em>.
+
       </p>
       <div className={SheetStyles.sideContent}>
         <img style={{position:'sticky', top:'calc(var(--navigation-height) + 1rem)'}} src={zoom_02}/>
@@ -88,43 +118,43 @@ export default class ShapeInformationSheet extends React.Component
       </div>
 
     <div className={SheetStyles.gridRow}>
-      <h3 className={SheetStyles.subtitleSheet}>Sintagmi e Preposizioni</h3>
-      <p className={SheetStyles.paragraph}>Un dato trasversale all’intero
-      corpus, che colpisce per la costanza con cui si mantiene stabile su
-      un’estensione di oltre quarant’anni che attraversa generi e stili molto
-      diversi, è la netta preferenza di Calvino per una procedura elencatoria
-      che coinvolga elementi complessi come  <span style={{color:'#ffa500'}}>sintagmi</span> e addirittura intere
-      <span style={{color:'#5151fc'}}> proposizioni</span>, gesto che, in termini retorici, costituisce il polo opposto
-      all’<em>accumulatio</em>, vale a dire la <em>distributio</em>: quel formato enumerativo in
-      cui la ripetizione non si limita alla ripresa di un oggetto grammaticale,
-      ma coinvolge delle porzioni di testo anche molto estese, la cui
-      riconoscibilità seriale non deriva solo dal piano sintattico ma anche da
-      quello semantico.</p>
+      <h2 className={SheetStyles.titleH2}>Sintagmi e Preposizioni</h2>
+      <p className={SheetStyles.paragraph}>Un dato trasversale all’intero corpus,
+      che colpisce per la costanza con cui si
+      mantiene stabile su un’estensione di oltre quarant’anni che attraversa generi e
+      stili molto diversi, è la netta preferenza di Calvino per una procedura
+      elencatoria che coinvolga elementi complessi come <span
+      style={{color:'#ffa500'}}>sintagmi</span> e addirittura intere <span
+      style={{color:'#5151fc'}}> proposizioni</span>, gesto che, in termini retorici,
+      costituisce il polo opposto all’<em>accumulatio</em>, vale a dire la
+      <em>distributio</em>: quel formato enumerativo in cui la ripetizione non si
+      limita alla ripresa di un oggetto grammaticale, ma coinvolge delle porzioni di
+      testo anche molto estese, la cui riconoscibilità seriale non deriva solo dal
+      piano sintattico ma anche da quello semantico.</p>
       </div>
     <div className={SheetStyles.gridRow}>
       <img className={SheetStyles.image100w} src={zoom_03}/>
       <br />
       <img className={SheetStyles.image100w} src={zoom_04}/>
       <p className={SheetStyles.paragraph}>
-      Questo dato rappresenta bene l’elemento di ambiguità
-      presente nella poetica calviniana: nello stesso punto in cui si registra
-      la più forte tendenza a far proliferare l’elemento orizzontale dell’elenco
-      a discapito dell’elemento verticale dell’architettura compositiva, con un
-      gesto che sembrerebbe suggerire una volontà di semplificazione e di sfida
-      alla struttura classica dell’arco drammatico (Freytag 1990), si manifesta
-      anche la massima ricerca di complessità nella costruzione delle unità
-      elencate, che in alcuni casi si espandono e articolano fin quasi a
-      oscurare la riconoscibilità dell’elenco in quanto tale. Ma questi elenchi,
-      e quelli di proposizioni in particolare, sono anche un segnale importante
-      delle scelte estetiche generali dell’autore, perché cancellando i nessi
-      causali a favore della semplice sequenza temporale (in cui un evento è
-      raccontato mettendo in fila i singoli momenti che lo compongono, come un
-      film che venga mostrato un fermo-immagine alla volta) rievocano
-      l’andamento delle cronache e degli annali, forme tradizionalmente
-      considerate opposte all’arte narrativa perché prive di trama, e in questo
-      modo contribuiscono a posizionare Calvino in una lunga tradizione di
-      letteratura «non-aristotelica» (Orr 1991) che risale fino a Sterne.
-      </p>
+      Questo dato rappresenta bene l’elemento di ambiguità presente nella poetica
+      calviniana: nello stesso punto in cui si registra la più forte tendenza a far
+      proliferare l’elemento orizzontale dell’elenco a discapito dell’elemento
+      verticale dell’architettura compositiva, con un gesto che sembrerebbe suggerire
+      una volontà di semplificazione e di sfida alla struttura classica dell’arco
+      drammatico (Freytag 1990), si manifesta anche la massima ricerca di complessità
+      nella costruzione delle unità elencate, che in alcuni casi si espandono e
+      articolano fin quasi a oscurare la riconoscibilità dell’elenco in quanto tale.
+      Ma questi elenchi, e quelli di proposizioni in particolare, sono anche un
+      segnale importante delle scelte estetiche generali dell’autore, perché
+      cancellando i nessi causali a favore della semplice sequenza temporale (in cui
+      un evento è raccontato mettendo in fila i singoli momenti che lo compongono,
+      come un film che venga mostrato un fermo-immagine alla volta) rievocano
+      l’andamento delle cronache e degli annali, forme tradizionalmente considerate
+      opposte all’arte narrativa perché prive di trama, e in questo modo
+      contribuiscono a posizionare Calvino in una lunga tradizione di letteratura
+      «non-aristotelica» (Orr 1991) che risale fino a Sterne.
+</p>
     </div>
 
     <div className={SheetStyles.gridRow}>
@@ -152,9 +182,9 @@ export default class ShapeInformationSheet extends React.Component
 
 
       <div className={SheetStyles.gridRow}>
-        <div style={{gridColumn:'1 / span 3'}}><ArrowButton arrowDirection="none" textAlign="left" text="ANALISI" route="/space/phase2" /></div>
-        <div style={{gridColumn:'4 / span 2'}}><ArrowButton arrowDirection="left" textAlign="right" text="TAPPA 1" route="/space/phase1"/></div>
-        <div style={{gridColumn:'6 / span 2'}}><ArrowButton arrowDirection="right" textAlign="left" text="TAPPA 3" route="/space/phase3" /></div>
+        <div style={{gridColumn:'1 / span 3'}}><ArrowButton arrowDirection="none" textAlign="left" text="ANALISI" route="/shape/phase2" /></div>
+        <div style={{gridColumn:'4 / span 2'}}><ArrowButton arrowDirection="left" textAlign="right" text="TAPPA 1" route="/shape/phase1"/></div>
+        <div style={{gridColumn:'6 / span 2'}}><ArrowButton arrowDirection="right" textAlign="left" text="TAPPA 3" route="/shape/phase3" /></div>
       </div>
     </main>
   </>
