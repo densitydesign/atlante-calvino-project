94e92d9653851967b174895aa5206822a467f79f
Beatrice Gobbo <gobbo.beatrice92@gmail.com>
2020-11-09 18:49:03 +0100

    Add LInk Anchors pack

--- src/views/ToolsAndMethods/ToolsAndMethods.js
+++ src/views/ToolsAndMethods/ToolsAndMethods.js
@@ -8,6 +8,7 @@ import "./ToolsAndMethods.css";
 import "../../general/GridsWithScrollableColumns/GridWithScrollableColumn.css";
 import "../../general/GridsWithScrollableColumns/ScrollableColumn.css";
 import IndexMenuHeader from "../../headers/IndexMenuHeader";
+import { HashLink as Link } from "react-router-hash-link";
 
 export default class ToolsAndMethods extends React.Component {
   render() {
@@ -16,10 +17,17 @@ export default class ToolsAndMethods extends React.Component {
         <IndexMenuHeader menuAction={"openIndex"} />
         <div className="ac-grid-24 has-sidebar">
           <div className="sidebarLeft sticky-element">
-            <h1>Capta</h1>
+            <h2 style={{ margin: "0.7142857143rem 0" }}>
+              Elenco dei capta spiegati in dettaglio.{" "}
+            </h2>
+            <h3>Clicca su uno dei seguenti... </h3>
             <ol>
-              <li>Capta Uno</li>
-              <li>Capta Uno</li>
+              <li>
+                <Link to="/capta#CaptaUno">Capta Uno</Link>
+              </li>
+              <li>
+                <Link to="/capta#CaptaDue">Capta Due</Link>
+              </li>
               <li>Capta Uno</li>
               <li>Capta Uno</li>
             </ol>
@@ -51,40 +59,61 @@ export default class ToolsAndMethods extends React.Component {
               </span>
             </p>
 
-            <br />
-            <h2>DensityDesign Research Lab</h2>
-            <h3 style={{ color: "black" }}>
-              Dipartimento di Design
-              <br />
-              Politecnico di Milano
-            </h3>
-            <p>
-              Il laboratorio di ricerca
-              <a href="http://densitydesign.org/" rel="noopener noreferrer">
-                DensityDesign
-              </a>
-              è parte del Dipartimento di Design del Politecnico di Milano.
-              Obiettivo del laboratorio è la rappresentazione di fenomeni
-              complessi di natura sociale, organizzativa o urbana. A questi temi
-              va aggiunto il crescente interesse per le Digital Humanities ed in
-              particolare per tutto ciò che sta all’intersezione fra le materie
-              umanistiche e l’information design, come la progettazione di
-              artefatti e metodi digitali per l’elaborazione, l’accesso e la
-              visualizzazione dell’informazione.
-            </p>
+            <div id="CaptaUno">
+              <h2>Corpus</h2>
+              <p>
+                La casa editrice italiana Mondadori, che detiene i diritti sul
+                lavoro di Italo Calvino, ha collaborato al progetto fornendo
+                l’opera completa nella collana “I Meridiani”, che si compone in
+                questo modo: l’opera narrativa integrale nei 3 volumi intitolati
+                Romanzi e racconti; le Fiabe italiane curate dall’autore nel
+                1956; un’ampia parte della produzione saggistica pubblicata nei
+                2 volumi dei Saggi; la selezione di epistole pubblicata nel
+                volume singolo Lettere; l’Album Calvino. Per realizzare i tre
+                Itinerari abbiamo deciso di focalizzarci unicamente sull’opera
+                narrativa pubblicata mentre Calvino era in vita. Il corpus di
+                riferimento si compone di oltre venti volumi distribuiti tra il
+                1946 e il 1984: romanzi, raccolte di racconti, forme ibride a
+                metà strada tra romanzi brevi e racconti lunghi, per un totale
+                di 206 titoli (vedi Istruzioni per l’uso). Il dataset è composto
+                da due tabelle relazionali che racchiudono le informazioni
+                riguardanti la selezione dei testi studiati in questo progetto.
+                Le informazioni provengono: dalla sezione Note e notizie sui
+                testi a cura di Mario Barenghi, Bruno Falcetto e Claudio
+                Milanini all’interno dei tre volumi Romanzi e racconti (I pp.
+                1242-1393; II pp. 1309-1475; III 1195-1350); dalla Bibliografia
+                di Italo Calvino di Luca Baranelli (Pisa, Scuola Normale
+                Superiore, 2007).
+              </p>
+            </div>
 
-            <br />
-            <p>
-              Allo sviluppo delle visualizzazioni hanno collaborato Mauro
-              Bianchi, Giovanni Fumagalli e Matteo Scaffidi di
-              <a href="https://inmagik.com/" rel="noopener noreferrer">
-                Inmagik
-              </a>
-              .
-              <br />
-              Ringraziamo Lorenzo Faggi per la registrazione, presso lo spazio
-              occupato Piano Terra, dell’audio del video introduttivo
-            </p>
+            <div id="CaptaDue">
+              <h2>Corpus2</h2>
+              <p>
+                1-Corpus La casa editrice italiana Mondadori, che detiene i
+                diritti sul lavoro di Italo Calvino, ha collaborato al progetto
+                fornendo l’opera completa nella collana “I Meridiani”, che si
+                compone in questo modo: l’opera narrativa integrale nei 3 volumi
+                intitolati Romanzi e racconti; le Fiabe italiane curate
+                dall’autore nel 1956; un’ampia parte della produzione saggistica
+                pubblicata nei 2 volumi dei Saggi; la selezione di epistole
+                pubblicata nel volume singolo Lettere; l’Album Calvino. Per
+                realizzare i tre Itinerari abbiamo deciso di focalizzarci
+                unicamente sull’opera narrativa pubblicata mentre Calvino era in
+                vita. Il corpus di riferimento si compone di oltre venti volumi
+                distribuiti tra il 1946 e il 1984: romanzi, raccolte di
+                racconti, forme ibride a metà strada tra romanzi brevi e
+                racconti lunghi, per un totale di 206 titoli (vedi Istruzioni
+                per l’uso). Il dataset è composto da due tabelle relazionali che
+                racchiudono le informazioni riguardanti la selezione dei testi
+                studiati in questo progetto. Le informazioni provengono: dalla
+                sezione Note e notizie sui testi a cura di Mario Barenghi, Bruno
+                Falcetto e Claudio Milanini all’interno dei tre volumi Romanzi e
+                racconti (I pp. 1242-1393; II pp. 1309-1475; III 1195-1350);
+                dalla Bibliografia di Italo Calvino di Luca Baranelli (Pisa,
+                Scuola Normale Superiore, 2007).
+              </p>
+            </div>
           </div>
         </div>
         <Footer />
