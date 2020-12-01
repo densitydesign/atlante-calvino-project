715cd2ae6c6ad0140b897906a80c156c5faeaf6b
skaffo <matteo.scaffidi@gmail.com>
2020-12-01 13:55:03 +0100

    Change combinare slider style

--- src/visualizations/Trama/MarimekkoSliderNative.js
+++ src/visualizations/Trama/MarimekkoSliderNative.js
@@ -1,24 +1,20 @@
-import React, {
-  useEffect,
-  useMemo,
-  useCallback,
-  useState,
-  useRef
-} from "react";
-import styles from "./Trama.module.css";
-import useDimensions from "react-use-dimensions";
-import { scaleLinear } from "d3";
-import Draggable from "react-draggable"; // The default
-import findLastIndex from "lodash/findLastIndex";
-import findIndex from "lodash/findIndex";
-import { findAtChar } from "./helpers";
-import sortBy from "lodash/sortBy";
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
-import "./Slider.css";
+import React, { useEffect, useMemo, useCallback, useState, useRef } from "react"
+import styles from "./Trama.module.css"
+import useDimensions from "react-use-dimensions"
+import { scaleLinear } from "d3"
+import Draggable from "react-draggable" // The default
+import findLastIndex from "lodash/findLastIndex"
+import findIndex from "lodash/findIndex"
+import { findAtChar } from "./helpers"
+import sortBy from "lodash/sortBy"
+import Slider from "rc-slider"
+import "rc-slider/assets/index.css"
+import "./Slider.css"
+import { ReactComponent as DownIcon } from "./icons/chevron-down.svg"
+import { ReactComponent as UpIcon } from "./icons/chevron-up.svg"
 
-const SLIDER_WIDTH = 30;
-const CURSOR_HEIGHT = 12;
+const SLIDER_WIDTH = 32
+const CURSOR_HEIGHT = 12
 
 export function MarimekkoSlider({
   currentPosition,
@@ -27,32 +23,32 @@ export function MarimekkoSlider({
   iceCycleData,
   setCurrentSequences,
   setCurrentSequencesSelected,
-  currentSequencesSelected
+  currentSequencesSelected,
 }) {
-  const [ref, { x, y, width, height }] = useDimensions();
+  const [ref, { x, y, width, height }] = useDimensions()
 
-  const sliderX = useMemo(() => width / 2 - SLIDER_WIDTH / 2, [width]);
+  const sliderX = useMemo(() => width / 2 - SLIDER_WIDTH / 2, [width])
 
   const yScale = useMemo(() => {
     return scaleLinear()
       .range([0, height - CURSOR_HEIGHT])
-      .domain([0, +currentBook.caratteri]);
-  }, [height, currentBook]);
+      .domain([0, +currentBook.caratteri])
+  }, [height, currentBook])
 
   const handleDrag = useCallback(
-    e => {
-      const delta = e.movementY;
+    (e) => {
+      const delta = e.movementY
       if (!delta) {
-        return;
+        return
       }
-      const deltaPosition = yScale.invert(delta);
-      const newPosition = currentPosition + deltaPosition;
+      const deltaPosition = yScale.invert(delta)
+      const newPosition = currentPosition + deltaPosition
       const safePosition = parseInt(
         Math.max(Math.min(newPosition, +currentBook.caratteri), 0)
-      );
+      )
       if (safePosition !== currentPosition) {
         //setCurrentSequencesSelected([]);
-        setCurrentPosition(safePosition);
+        setCurrentPosition(safePosition)
       }
     },
     [
@@ -60,20 +56,20 @@ export function MarimekkoSlider({
       currentPosition,
       setCurrentPosition,
       setCurrentSequencesSelected,
-      yScale
+      yScale,
     ]
-  );
+  )
 
   const handleDragNative = useCallback(
-    h => {
-      const deltaPosition = yScale.invert(h);
-      const newPosition = deltaPosition;
+    (h) => {
+      const deltaPosition = yScale.invert(h)
+      const newPosition = deltaPosition
       const safePosition = parseInt(
         Math.max(Math.min(newPosition, +currentBook.caratteri), 0)
-      );
+      )
       if (safePosition !== currentPosition) {
         //setCurrentSequencesSelected([]);
-        setCurrentPosition(safePosition);
+        setCurrentPosition(safePosition)
       }
     },
     [
@@ -81,78 +77,76 @@ export function MarimekkoSlider({
       currentPosition,
       setCurrentPosition,
       setCurrentSequencesSelected,
-      yScale
+      yScale,
     ]
-  );
+  )
 
   const cursorY = useMemo(() => {
-    return Math.max(yScale(currentPosition), 0);
-  }, [yScale, currentPosition]);
+    return Math.max(yScale(currentPosition), 0)
+  }, [yScale, currentPosition])
 
   const currentSequences = useMemo(() => {
     const out = iceCycleData.filter(
-      item =>
+      (item) =>
         item.starts_at <= currentPosition && item.ends_at >= currentPosition
-    );
-    return out;
-  }, [iceCycleData, currentPosition]);
+    )
+    return out
+  }, [iceCycleData, currentPosition])
 
   useEffect(() => {
-    setCurrentSequences(currentSequences);
-  }, [currentSequences, setCurrentSequences]);
+    setCurrentSequences(currentSequences)
+  }, [currentSequences, setCurrentSequences])
 
   const selected = useMemo(() => {
-    return currentSequencesSelected && currentSequencesSelected.length;
-  }, [currentSequencesSelected]);
+    return currentSequencesSelected && currentSequencesSelected.length
+  }, [currentSequencesSelected])
 
-  const dragging = useRef(0);
+  const dragging = useRef(0)
 
   return (
-    <div
-      
-      ref={ref}
-      className={`text-center position-relative ${styles.slider}`}
-    >
-      {height > 0 && <>
-      <Slider
-        reverse
-        vertical
-        min={0}
-        max={Math.floor(height - CURSOR_HEIGHT)}
-        style={{ height: height - CURSOR_HEIGHT }}
-        step={1}
-        defaultValue={0}
-        value={cursorY}
-        onChange={handleDragNative}
-      />
-      {((currentSequences && currentSequences.length > 0) ||
-        (currentSequencesSelected && currentSequencesSelected.length > 0)) >
-        0 && (
-        <div
-          onClick={() => {
-            if (selected) {
-              setCurrentSequencesSelected([]);
-            } else if (currentSequences && currentSequences.length) {
-              setCurrentSequencesSelected(currentSequences);
-            }
-          }}
-          style={{
-            position: "absolute",
-            top: cursorY - 1,
-            left: -30,
-            cursor: "pointer",
-            width: CURSOR_HEIGHT,
-            height: CURSOR_HEIGHT,
-            border: "solid #222 1px",
-            borderRadius: 20,
-            background: selected ? "#222" : undefined,
-            borderColor: `var(--dark-blue)`
-          }}
-        ></div>
+    <div ref={ref} className={`text-center position-relative ${styles.slider}`}>
+      {height > 0 && (
+        <>
+          <Slider
+            reverse
+            vertical
+            min={0}
+            max={Math.floor(height - CURSOR_HEIGHT)}
+            style={{ height: height - CURSOR_HEIGHT }}
+            step={1}
+            defaultValue={0}
+            value={cursorY}
+            onChange={handleDragNative}
+          />
+          {((currentSequences && currentSequences.length > 0) ||
+            (currentSequencesSelected && currentSequencesSelected.length > 0)) >
+            0 && (
+            <div
+              onClick={() => {
+                if (selected) {
+                  setCurrentSequencesSelected([])
+                } else if (currentSequences && currentSequences.length) {
+                  setCurrentSequencesSelected(currentSequences)
+                }
+              }}
+              style={{
+                position: "absolute",
+                top: cursorY - 1,
+                left: -30,
+                cursor: "pointer",
+                width: CURSOR_HEIGHT,
+                height: CURSOR_HEIGHT,
+                border: "solid #222 1px",
+                borderRadius: 20,
+                background: selected ? "#222" : undefined,
+                borderColor: `var(--dark-green)`,
+              }}
+            ></div>
+          )}
+        </>
       )}
-      </>}
     </div>
-  );
+  )
 }
 
 export function MarimekkoSliderArrow({
@@ -161,13 +155,13 @@ export function MarimekkoSliderArrow({
   nextPosition,
   setCurrentPosition,
   up = false,
-  down = false
+  down = false,
 }) {
-  const [ref, { x, y, width, height }] = useDimensions();
-  const sliderX = useMemo(() => width / 2 - SLIDER_WIDTH / 2, [width]);
+  const [ref, { x, y, width, height }] = useDimensions()
+  const sliderX = useMemo(() => width / 2 - SLIDER_WIDTH / 2, [width])
 
   if (!currentBook) {
-    return null;
+    return null
   }
 
   return (
@@ -175,28 +169,42 @@ export function MarimekkoSliderArrow({
       {width && up && (
         <button
           disabled={prevPosition === null}
-          style={{ left: sliderX, bottom: 0, width: SLIDER_WIDTH }}
-          className={`btn position-absolute text-center p-1 ${styles['border-dark-blue']}`}
+          style={{
+            left: sliderX,
+            bottom: 0,
+            width: SLIDER_WIDTH,
+            borderBottomLeftRadius: 0,
+            borderBottomRightRadius: 0,
+            background: 'white',
+            color: '#000'
+          }}
+          className={`btn position-absolute text-center p-1 ${styles["border-black"]}`}
           onClick={() =>
             prevPosition !== null && setCurrentPosition(prevPosition)
           }
         >
-          ↑
+          <UpIcon />
         </button>
       )}
       {width && down && (
         <button
           disabled={nextPosition === null}
-          className={`btn position-absolute text-center p-1 ${styles['border-dark-blue']}`}
-          
-          style={{ left: sliderX, width: SLIDER_WIDTH }}
+          className={`btn position-absolute text-center p-1 ${styles["border-black"]}`}
+          style={{
+            left: sliderX,
+            width: SLIDER_WIDTH,
+            borderTopRightRadius: 0,
+            borderTopLeftRadius: 0,
+            background: 'white',
+            color: '#000'
+          }}
           onClick={() =>
             nextPosition !== null && setCurrentPosition(nextPosition)
           }
         >
-          ↓
+          <DownIcon />
         </button>
       )}
     </div>
-  );
+  )
 }
