ac4360b4b3984abc70d2c12ab17278163da25598
skaffo <matteo.scaffidi@gmail.com>
2020-12-02 13:40:51 +0100

    Fixed legend marimekko

--- src/visualizations/Trama/MarimekkoLegend.js
+++ src/visualizations/Trama/MarimekkoLegend.js
@@ -1,7 +1,7 @@
-import React, { useState, useMemo, useRef, useEffect } from "react";
-import groupBy from "lodash/groupBy";
-import styles from "./Trama.module.css";
-import { useTranslation } from "react-i18next";
+import React, { useState, useMemo, useRef, useEffect } from "react"
+import groupBy from "lodash/groupBy"
+import styles from "./Trama.module.css"
+import { useTranslation } from "react-i18next"
 
 export default function MarimekkoLegend({
   selectedLegendEntries,
@@ -10,15 +10,15 @@ export default function MarimekkoLegend({
   dettaglio,
   currentBook,
 }) {
-  const { t } = useTranslation('combining')
+  const { t } = useTranslation("combining")
 
   const byGruppo = groupBy(
-    legendMap.filter(x => x.gruppo),
+    legendMap.filter((x) => x.gruppo),
     "gruppo"
-  );
+  )
   const anyLegendEntrySelected = Object.keys(
     selectedLegendEntries || {}
-  ).filter(k => selectedLegendEntries[k]).length;
+  ).filter((k) => selectedLegendEntries[k]).length
 
   return (
     <div
@@ -27,42 +27,57 @@ export default function MarimekkoLegend({
       }`}
     >
       <div className="">
-        <h4>{dettaglio === 'ambito' ? 'TIPO DI PIANO': 'TIPO DI SEQUENZA'}</h4>
+        <h4>{dettaglio === "ambito" ? "TIPO DI PIANO" : "TIPO DI SEQUENZA"}</h4>
       </div>
-      {Object.keys(byGruppo).map(gruppo => (
+      {Object.keys(byGruppo).map((gruppo) => (
         <div key={gruppo} className="mt-1">
-          <div className={styles.legendGroupText}>{t(gruppo)}</div>
+          {dettaglio !== "ambito" && (
+            <div className={styles.legendGroupText}>{t(gruppo)}</div>
+          )}
           <div>
             {byGruppo[gruppo].map((item, i) => {
-              const notFound = currentBook && !currentBook.categoriesMatchesForBook[item.valore]
+              const notFound =
+                currentBook &&
+                !currentBook.categoriesMatchesForBook[item.valore]
               return (
-              <div
-                key={i}
-                style={{filter: notFound ? 'saturate(0%)': undefined, opacity: notFound ? 0.3: undefined}}
-                className={`w-100 d-flex align-items-center ${
-                  styles.legendEntry
-                } ${selectedLegendEntries[item.valore] ? styles.selected : ""}`}
-              >
                 <div
-                  className={styles.legendEntryColor}
-                  style={{ backgroundColor: notFound ? '#ccc' : item.colore, cursor: notFound ? undefined: 'pointer' }}
-                  onClick={() => {
-                    if(notFound){
-                      return
-                    }
-                    const newEntries = {
-                      ...selectedLegendEntries,
-                      [item.valore]: !!!selectedLegendEntries[item.valore]
-                    };
-                    setSelectedLegendEntries(newEntries);
+                  key={i}
+                  style={{
+                    filter: notFound ? "saturate(0%)" : undefined,
+                    opacity: notFound ? 0.3 : undefined,
                   }}
-                ></div>
-                <span className={styles.legendEntryText}>{t(`${dettaglio}.${item.label || item.valore}`)}</span>
-              </div>
-            )})}
+                  className={`w-100 d-flex align-items-center ${
+                    styles.legendEntry
+                  } ${
+                    selectedLegendEntries[item.valore] ? styles.selected : ""
+                  }`}
+                >
+                  <div
+                    className={styles.legendEntryColor}
+                    style={{
+                      backgroundColor: notFound ? "#ccc" : item.colore,
+                      cursor: notFound ? undefined : "pointer",
+                    }}
+                    onClick={() => {
+                      if (notFound) {
+                        return
+                      }
+                      const newEntries = {
+                        ...selectedLegendEntries,
+                        [item.valore]: !!!selectedLegendEntries[item.valore],
+                      }
+                      setSelectedLegendEntries(newEntries)
+                    }}
+                  ></div>
+                  <span className={styles.legendEntryText}>
+                    {t(`${dettaglio}.${item.label || item.valore}`)}
+                  </span>
+                </div>
+              )
+            })}
           </div>
         </div>
       ))}
     </div>
-  );
+  )
 }
