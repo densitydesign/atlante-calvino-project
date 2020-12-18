e1edb256cf925109f269bd415ff1519461172fc0
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-12-18 14:15:39 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/atlante-calvino-project/precache-manifest.8fe0855a9915974e7b28d455fe383107.js"
+  "/atlante-calvino-project/precache-manifest.f4a6dda2ba1f840701f4f5761d576ca5.js"
 );
 
 self.addEventListener('message', (event) => {
