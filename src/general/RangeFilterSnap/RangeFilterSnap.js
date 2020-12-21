52e6f1dcaf2fe8d88c7af8926f44ef5f2dbe5e4e
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-12-21 10:17:47 +0100

    Update RangeFilterSnap.js

--- src/general/RangeFilterSnap/RangeFilterSnap.js
+++ src/general/RangeFilterSnap/RangeFilterSnap.js
@@ -22,37 +22,37 @@ export default function RangeFilterSnap({ extent, update, style }) {
       .domain(extent)
       .rangeRound([margin.left, width - margin.right]);
 
-    const xAxis = (g) =>
-      g
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call((g) =>
-          g
-            .append("g")
-            .call(
-              d3
-                .axisBottom(x)
-                .ticks(interval)
-                .tickSize(-height + margin.top + margin.bottom)
-                .tickFormat(() => null)
-            )
-            .call((g) =>
-              g.select(".domain").attr("fill", "#ddd").attr("stroke", null)
-            )
-            .call((g) =>
-              g
-                .selectAll(".tick line")
-                .attr("stroke", "#fff")
-                .attr("stroke-opacity", (d) => (d <= d3.timeDay(d) ? 1 : 0.5))
-            )
-        )
-        .call((g) =>
-          g
-            .append("g")
-            .call(d3.axisBottom(x).ticks(d3.timeYear.every(4)).tickPadding(0))
-            .attr("text-anchor", null)
-            .call((g) => g.select(".domain").remove())
-            .call((g) => g.selectAll("text").attr("x", 6))
-        );
+    // const xAxis = (g) =>
+    //   g
+    //     .attr("transform", `translate(0,${height - margin.bottom})`)
+    //     .call((g) =>
+    //       g
+    //         .append("g")
+    //         .call(
+    //           d3
+    //             .axisBottom(x)
+    //             .ticks(interval)
+    //             .tickSize(-height + margin.top + margin.bottom)
+    //             .tickFormat(() => null)
+    //         )
+    //         .call((g) =>
+    //           g.select(".domain").attr("fill", "#ddd").attr("stroke", null)
+    //         )
+    //         .call((g) =>
+    //           g
+    //             .selectAll(".tick line")
+    //             .attr("stroke", "#fff")
+    //             .attr("stroke-opacity", (d) => (d <= d3.timeDay(d) ? 1 : 0.5))
+    //         )
+    //     )
+    //     .call((g) =>
+    //       g
+    //         .append("g")
+    //         .call(d3.axisBottom(x).ticks(d3.timeYear.every(4)).tickPadding(0))
+    //         .attr("text-anchor", null)
+    //         .call((g) => g.select(".domain").remove())
+    //         .call((g) => g.selectAll("text").attr("x", 6))
+    //     );
 
     const brush = d3.brushX().extent([
       [margin.left, margin.top],
