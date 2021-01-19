237ef744ad8aaaa4268205a2967becdbeb6ecea1
angelesbri1s <angelesbriones@gmail.com>
2021-01-19 13:12:10 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/atlante-calvino-project/precache-manifest.cfd801b53465787fb671419aef892e9d.js"
+  "/atlante-calvino-project/precache-manifest.1bb823960cbf93c89363e1ac906eea5a.js"
 );
 
 self.addEventListener('message', (event) => {
