3947446471a80c2226d3941473546a6db9fb5d00
iosonosempreio <iosonosempreio@users.noreply.github.com>
2021-01-27 15:42:21 +0100

    Plot - tendina refusi

--- src/helpPages/plot/PlotMainHelp/PlotMainHelp.js
+++ src/helpPages/plot/PlotMainHelp/PlotMainHelp.js
@@ -193,9 +193,9 @@ export default function PlotMainHelp({ helpProps }) {
                     <small>{t("trama:help_panel.dimensione_e_colore")}</small>
                   </div>
                   {i18n.language === "it" ? (
-                    <LegendDimensione02 width="300" className="mt-2" />
+                    <LegendDimensione02 width="100%" className="mt-2" />
                   ) : (
-                    <LegendDimensione02En width="300" className="mt-2" />
+                    <LegendDimensione02En width="100%" className="mt-2" />
                   )}
                 </div>
               </div>
@@ -256,13 +256,6 @@ export default function PlotMainHelp({ helpProps }) {
                   "trama:ostacolo"
                 )}, ${t("trama:successo")}`}
               />
-              <BadgeLegenda
-                color="#00FFFF"
-                name={t("trama:Situazione")}
-                italic={`${t("trama:Cambiamento")}, ${t(
-                  "trama:città magica"
-                )}, ${t("trama:situazione")}`}
-              />
             </div>
           </div>
           <div className="row">
@@ -292,6 +285,13 @@ export default function PlotMainHelp({ helpProps }) {
                 color="#00FFB6"
                 name={t("trama:Stato sospeso")}
                 italic={`${t("trama:Pausa/sospensione")}, ${t("trama:attesa")}`}
+              />
+              <BadgeLegenda
+                color="#00FFFF"
+                name={t("trama:Situazione")}
+                italic={`${t("trama:Cambiamento")}, ${t(
+                  "trama:città magica"
+                )}, ${t("trama:situazione")}`}
               />
             </div>
           </div>
